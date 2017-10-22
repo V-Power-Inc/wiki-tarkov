472ace106c603ae231d4377b8ebd92007547d5f1
Василий <basil2305@ya.ru>
2017-10-22 20:22:11 +0400

    22-10-2017 - Кнопка - едем наверх, если чуть чуть отскролли страницу вниз

--- web/js/main.js
+++ web/js/main.js
@@ -1,3 +1,19 @@
 /**
  * Created by comp on 22.10.2017.
  */
+
+/** Скорлл кнопка до верхней части сайта **/
+
+$(document).ready(function() {
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('.scup').fadeIn();
+        } else {
+            $('.scup').fadeOut();
+        }
+    });
+    $('.scup').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 900);
+        return false;
+    });
+});
