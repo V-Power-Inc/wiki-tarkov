a13691209b1e7bde2e724f29dda629779141abd6
Василий <basil2305@ya.ru>
2017-10-24 18:21:31 +0400

    24-10-2017 - Сделана База данных с таблицей админов, создана модель Админов

---
+++ models/Admins.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "admins".
+ *
+ * @property int $id
+ * @property string $login 
+ * @property string $real_name
+ * @property string $passwd 
+ * @property string $confirm
+ */
+class Admins extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'admins';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['login', 'real_name', 'passwd', 'confirm'], 'required'],
+            [['login', 'real_name', 'passwd'], 'string', 'max' => 45],
+            [['confirm'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'login' => 'Логин',
+            'real_name' => 'Real Name',
+            'passwd' => 'Пароль',
+            'confirm' => 'Confirm',
+        ];
+    }
+}
