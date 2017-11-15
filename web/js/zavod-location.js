7eaccc9691f1cdac1de99bb6cac51daa9a594802
DIR300NRU-ADMIN (Toshiba) <basil2305@ya.ru>
2017-11-15 16:38:01 +0300

    15-11-2017  Запрещен скроллинг карты по всем направлениям кроме направления вниз

--- web/js/zavod-location.js
+++ web/js/zavod-location.js
@@ -5,7 +5,7 @@
 /** Вызов карты и указание центра координат **/
 const map = L.map('map', {
     center: [67, -40],
-    zoom: 3,
+    zoom: 2,
     maxzoom: 4,
     minzoom: 2,
 });
@@ -16,9 +16,17 @@ L.tileLayer('http://eft-locations.kfc-it.ru/img/zavod/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 /** Устанавливаем зум карты на 2 также указываем что минимальный зум 2 а максимальный 4 **/
-map.setZoom(3);
+map.setZoom(2);
 map.setMaxZoom(4);
 map.setMinZoom(2);
 
 
 /** Ограничение на перетягивание карты, если в экране есть края карты **/
+var southWest = L.latLng(-89.98155760646617, -180),
+    northEast = L.latLng(89.99346179538875, 80);
+var bounds = L.latLngBounds(southWest, northEast);
+
+map.setMaxBounds(bounds);
+map.on('drag', function() {
+    map.panInsideBounds(bounds, { animate: false });
+});
