be92cbfe21ff3fbe984e331052dc571ddcf96070
Василий <basil2305@ya.ru>
2017-12-22 20:34:13 +0400

    22-12-2017 - Микро. допил по русификации

--- modules/admin/views/forest/view.php
+++ modules/admin/views/forest/view.php
@@ -15,8 +15,8 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+        <?= Html::a('Обновить маркер', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Удалить маркер', ['delete', 'id' => $model->id], [
             'class' => 'btn btn-danger',
             'data' => [
                 'confirm' => 'Вы уверены что хотите удалить этот маркер?',
