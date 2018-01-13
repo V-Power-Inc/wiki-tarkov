7e3ae08131dba0c0578dcc38c24cb1d2b8183e3c
Василий <basil2305@ya.ru>
2018-01-13 19:30:58 +0400

    13-01-2018 - Подправил еще 1 архитектурный баг

--- web/js/adminscript.js
+++ web/js/adminscript.js
@@ -8,12 +8,16 @@ $( document ).ready(function() {
     if($('#forest-marker_group option:selected').text() === "Маркеры выходов") {
         $('.form-group.field-forest-exits_group').fadeIn();
         $('.form-group.field-forest-file').fadeIn();
+    } else if($('#forest-marker_group option:selected').text() !== "Маркеры выходов") {
+        $('.form-group.field-forest-exit_anyway').fadeOut();
+        $('#forest-exit_anyway').val('');
     }
 });
 $("#forest-marker_group").change(function() {
     if($('#forest-marker_group option:selected').text() === "Маркеры выходов") {
         $('.form-group.field-forest-exits_group').fadeIn();
         $('.form-group.field-forest-file').fadeIn();
+        $('.form-group.field-forest-exit_anyway').fadeIn();
     } else {
         $('.form-group.field-forest-exits_group').fadeOut();
         $('.form-group.field-forest-file').fadeOut();
