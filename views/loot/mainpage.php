8cc1298364e17f058979897f3706a26f2a87a4fd
Василий <basil2305@ya.ru>
2018-01-26 20:12:09 +0400

    26-01-2018 - Последние доработки по верстке вертикального меню

--- views/loot/mainpage.php
+++ views/loot/mainpage.php
@@ -22,7 +22,7 @@ $this->registerJsFile('js/lootscripts/mainloot.js', ['depends' => [\yii\web\Jque
 <div class="container">
     <div class="row">
         <!-- Меню левой части страницы -->
-        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+        <div class="col-lg-3 col-md-4 col-sm-12 col-xs-12">
             <ul class="nav nav-pills nav-stacked categories categories-menu" id="categories-menu">
                 <li class="relative active"><a href="#">Оружие</a></li>
                 <li class="relative">
@@ -30,9 +30,9 @@ $this->registerJsFile('js/lootscripts/mainloot.js', ['depends' => [\yii\web\Jque
                     <div class="dcjq-icon">&nbsp;&nbsp;&nbsp;</div>
                         <!-- Подкатегории меню -->
                         <ul class="children-cats">
-                            <li class="level-2"><a href="#">Военное снаряжение</a></li>
-                            <li class="level-2"><a href="#">Обвесы</a></li>
-                            <li class="level-2"><a href="#">Ранцы</a></li>
+                            <li class="level-2"><i class="fa fa-check-circle" aria-hidden="true"></i><a href="#">Военное снаряжение</a></li>
+                            <li class="level-2"><i class="fa fa-check-circle" aria-hidden="true"></i><a href="#">Обвесы</a></li>
+                            <li class="level-2"><i class="fa fa-check-circle" aria-hidden="true"></i><a href="#">Ранцы</a></li>
                         </ul>
                         <!-- Окончание подкатегорий -->
                 </li>
@@ -45,9 +45,9 @@ $this->registerJsFile('js/lootscripts/mainloot.js', ['depends' => [\yii\web\Jque
                     <div class="dcjq-icon">&nbsp;&nbsp;&nbsp;</div>
                         <!-- Подкатегории меню -->
                         <ul class="children-cats">
-                            <li class="level-2"><a href="#">Подкатегория 1</a></li>
-                            <li class="level-2"><a href="#">Подкатегория 2</a></li>
-                            <li class="level-2"><a href="#">Подкатегория 3</a></li>
+                            <li class="level-2"><i class="fa fa-check-circle" aria-hidden="true"></i><a href="#">Подкатегория 1</a></li>
+                            <li class="level-2"><i class="fa fa-check-circle" aria-hidden="true"></i><a href="#">Подкатегория 2</a></li>
+                            <li class="level-2"><i class="fa fa-check-circle" aria-hidden="true"></i><a href="#">Подкатегория 3</a></li>
                         </ul>
                         <!-- Окончание подкатегорий -->
                 </li>
@@ -56,7 +56,7 @@ $this->registerJsFile('js/lootscripts/mainloot.js', ['depends' => [\yii\web\Jque
         </div>
 
         <!-- Основное содержимое страницы -->
-        <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 quests-content">
+        <div class="col-lg-9 col-md-8 col-sm-12 col-xs-12 quests-content">
            <p class="alert alert-info size-16">На этой странице вы можете узнать информацию о любом луте из игры Escape from Tarkov. В справочнике вы сможете найти информацию о любом внутриигровом предмете. <br><br>
            Для удобства была создана разбивка по категориям, что облегчит вам поиск наиболее интересных предметов.</p>
         </div>
