2de04fc556d9bbcf19ae5b43ae90138cad9d887b
DIR300NRU-ADMIN (Toshiba) <basil2305@ya.ru>
2018-01-29 12:02:54 +0300

    29-01-2018 - Реализованы адекватные проверки активности категорий в виджете бокового меню

--- components/render_views/leftmenu.php
+++ components/render_views/leftmenu.php
@@ -1,15 +1,15 @@
 <!-- 1 уровень вложенности -->
 <?php if(!isset($category['childs'])) : ?>
-<li class="relative <?= (stristr(Yii::$app->request->url, $category['url'])) ? 'active' : '' ?>"><a href="/loot/<?= $category['url'] ?>"><?= $category['title'] ?></a>
+<li class="relative <?= (ltrim(strrchr(Yii::$app->request->url,'/'),'/') == $category['url']) ? 'active' : '' ?>"><a href="/loot/<?= $category['url'] ?>"><?= $category['title'] ?></a>
 <?php endif; ?>
 
 <!-- 2 уровень вложенности -->    
 <?php if(isset($category['childs'])) : ?>
-    <li class="relative <?= (stristr(Yii::$app->request->url, $category['url'])) ? 'active' : '' ?>"><a href="/loot/<?= $category['url'] ?>"><?= $category['title'] ?></a>
+    <li class="relative  <?= (stristr(Yii::$app->request->url, $category['url'])) ? 'active' : '' ?>"><a href="/loot/<?= $category['url'] ?>"><?= $category['title'] ?></a>
     <div class="dcjq-icon">&nbsp;&nbsp;&nbsp;</div>
     <ul class="children-cats">
         <?php foreach($category['childs'] as $childCategory) : ?>
-        <li class="level-2"><i class="fa fa-check-circle" aria-hidden="true"></i><a href="/loot/<?= $category['url'] ?>/<?= $childCategory['url'] ?>"><?= $childCategory['title'] ?></a></li>
+        <li class="level-2 <?= (ltrim(strrchr(Yii::$app->request->url,'/'),'/') == $childCategory['url']) ? 'active' : '' ?>"><i class="fa fa-check-circle" aria-hidden="true"></i><a href="/loot/<?= $category['url'] ?>/<?= $childCategory['url'] ?>"><?= $childCategory['title'] ?></a></li>
         <?php endforeach; ?>
     </ul>
 <?php endif; ?>
