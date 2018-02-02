5ec528b33f57627a09409c71d68228caef5dc009
DIR300NRU-ADMIN (Toshiba) <basil2305@ya.ru>
2018-02-02 16:42:10 +0300

    02-02-2018 - Закинул файл урл менеджера для каткгорий (Компонент)

---
+++ components/CategoryurlComponent.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DIR300NRU-ADMIN
+ * Date: 02.02.2018
+ * Time: 16:40
+ */
+
+namespace app\components;
+
+use yii\web\UrlRule;
+use app\models\Category;
+use yii\web\UrlRuleInterface;
+
+
+class PathRule implements UrlRuleInterface
+{
+/** Урл компонент для маршрутизации каткгорий справочника лута **/
+    public function parseRequest($manager, $request)
+    {
+
+        $path = $rest = substr($request->pathInfo, 0, strpos($request->pathInfo,'.'));
+        // если это простой url тоесть из вертикального меню
+        if($request->pathInfo === ''){
+            return ['articles/index',[]];
+        }elseif(strpos($request->pathInfo,'/') !== false){
+            if (preg_match('%^([\w\-]+)([\/]{1})([\-\w\d]+)$%', $path, $matches)){
+                return ['articles/fulltext',['name'=>$matches[3]]];
+            }
+        }
+
+        return false;
+    }
+
+    public function createUrl($manager, $route, $params)
+    {
+        $pathInfo = $route;
+        if (preg_match('%^(([\d]+)([\/]?)([\-\d\w]+))$%', $pathInfo, $matches)) {
+            $catigories = Category::find()->select('cat_url')->where(['id'=>$matches[2]])->scalar();
+            return $catigories.'/'.$matches[4].'.html';
+        }
+        return false;  // данное правило не применимо
+    }
+}
