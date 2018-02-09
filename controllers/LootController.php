a99d4f077ddc50c5dc23713232c05c767a5b4b9d
DIR300NRU-ADMIN (Toshiba) <basil2305@ya.ru>
2018-02-09 16:39:50 +0300

    09-02-2018 - Запрос вывода предметов из корневой и дочерних категорий построен

--- controllers/LootController.php
+++ controllers/LootController.php
@@ -14,6 +14,7 @@ use app\models\Category;
 use app\models\Items;
 use yii\data\Pagination;
 use yii\web\HttpException;
+use yii\db\Query;
 
 
 class LootController extends Controller
@@ -38,9 +39,25 @@ class LootController extends Controller
         
         if($cat) {
             // Тут надо получить предметы из всех дочерних категорий если мы находимся в родительской
+            $customers = Items::find()
+                ->alias( 'i' )
+                ->select('`i`.*,c1.*')
+                ->leftJoin('category as c1', '`i`.`parentcat_id` = `c1`.`id`')
+                ->leftJoin('category as c2', '`c2`.`parent_category` IS NULL')
+                ->where(['c1.title' => 'Экипировка'])
+->orWhere(['c1.title' => 'Оружие'])
+                ->with('parentcat')
+                ->all();
+            echo "<pre>";
+            print_r($customers);
+            echo "<pre>";
+            exit();
             $fullitems = Items::find()->where(['parentcat_id' => $cat['id']])->andWhere(['active' => 1]);
+           
             $pagination = new Pagination(['defaultPageSize' => 1,'totalCount' => $fullitems->count(),]);
             $items = $fullitems->offset($pagination->offset)->orderby(['date_create'=>SORT_DESC])->limit($pagination->limit)->all();
+           
+         
             $request = \Yii::$app->request;
             return $this->render('categorie-page.php', ['cat' => $cat, 'items' => $items, 'active_page' => $request->get('page',1),'count_pages' => $pagination->getPageCount(), 'pagination' => $pagination,]);
         } else {
