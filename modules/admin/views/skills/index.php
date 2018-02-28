9499d21e51b6434eae197f5fbf132df95ae41285
Василий <basil2305@ya.ru>
2018-02-28 16:52:22 +0400

    28-02-2018 - Сделано напоминание в админке о том что отключенное умение в справочнике умений - будет отображаться но на детальную страницу попасть будет нельзя

--- modules/admin/views/skills/index.php
+++ modules/admin/views/skills/index.php
@@ -15,8 +15,15 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="skills-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
+
+    <p class="alert alert-danger size-16 margin-bottom-5">
+        <b>Внимание</b>- Если умение отключено, оно будет выведено в списке умений на сайте, но перейти на его детальную страницу будет нельзя, также будет написано что умение еще не доступно в игре.</p>
+    <span class="label label-important">Важные особенности работы!</span>
+
+    <br>
+    <br>
+    
     <p>
         <?= Html::a('Создать новое умение', ['create'], ['class' => 'btn btn-success']) ?>
         <a class="btn btn-primary" href="/admin/">Вернуться на главную в админку</a>
