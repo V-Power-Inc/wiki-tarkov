e1d9ba1e40a6defc111d736e721ccb275d2856de
Василий <basil2305@ya.ru>
2018-04-21 09:12:42 +0400

    21-04-2018 - Код рекламы яндекс директа - вынесен в отдельный файл и генерится через $this->render('other/yandex-direct.php');

---
+++ views/other/yandex-direct.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: comp
+ * Date: 21.04.2018
+ * Time: 7:59
+ * View of Yandex Direct - used as compact short link in main views with render
+ */
+?>
+
+<!-- Yandex.RTB R-A-267537-1 -->
+<div id="yandex_rtb_R-A-267537-1"></div>
+<script type="text/javascript">
+    (function(w, d, n, s, t) {
+        w[n] = w[n] || [];
+        w[n].push(function() {
+            Ya.Context.AdvManager.render({
+                blockId: "R-A-267537-1",
+                renderTo: "yandex_rtb_R-A-267537-1",
+                async: true
+            });
+        });
+        t = d.getElementsByTagName("script")[0];
+        s = d.createElement("script");
+        s.type = "text/javascript";
+        s.src = "//an.yandex.ru/system/context.js";
+        s.async = true;
+        t.parentNode.insertBefore(s, t);
+    })(this, this.document, "yandexContextAsyncCallbacks");
+</script>
+
