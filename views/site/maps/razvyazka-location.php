1e6910196c431eb2fca78b7d5b782d642d668a85
Василий <basil2305@ya.ru>
2018-06-17 12:41:27 +0400

    17-06-2018 - Готов временный вариант локации развязка (Обновить ее - после того как разрабы запилят нормальную)

--- views/site/maps/razvyazka-location.php
+++ views/site/maps/razvyazka-location.php
@@ -6,10 +6,10 @@
  * Time: 15:00
  */
 
-// $this->registerCssFile("js/leaflet/leaflet.css", ['depends' => ['app\assets\AppAsset']]);
-// $this->registerJsFile('js/leaflet/leaflet.js', ['depends' => [\yii\web\JqueryAsset::className()]]);
-// $this->registerJsFile('js/map_hash.js', ['depends' => [\yii\web\JqueryAsset::className()]]);
-// $this->registerJsFile('js/razvyazka-location.js', ['depends' => [\yii\web\JqueryAsset::className()]]);
+$this->registerCssFile("js/leaflet/leaflet.css", ['depends' => ['app\assets\AppAsset']]);
+$this->registerJsFile('js/leaflet/leaflet.js', ['depends' => [\yii\web\JqueryAsset::className()]]);
+$this->registerJsFile('js/map_hash.js', ['depends' => [\yii\web\JqueryAsset::className()]]);
+$this->registerJsFile('js/razvyazka-location.js', ['depends' => [\yii\web\JqueryAsset::className()]]);
 $this->title = 'Карта локации Развязка в Escape from Tarkov - интерактивная карта со спавнами Диких, точками военных ящиков и ключей';
 $this->registerMetaTag([
     'name' => 'description',
@@ -25,59 +25,51 @@ $this->registerMetaTag([
 
 <!-- Инициализация карты -->
 <div class="w-100">
-    <!-- todo: Убрать ручные стили после релиза -->
-    <div id="map" class="map relative" style="background: #e7dfdd; margin-top: -10px;">
-        
-        <!-- NetCat div block -->
-        <div style="width:400px; height:300px; position:absolute;top:50%;left:50%;margin:-200px 0 0 -150px;">
-            <img src="/img/netcat.gif">
-        </div>
+    <div id="map" class="map relative">
         
         <!-- Координаты мышки -->
         <div id="mapCoords" data-original-title="" title=""></div>
         <!-- Кнопка вернуться к центру -->
-        <!-- todo: Убрать disavled класс после релиза -->
-        <p class="mapcenter"><a class="btn btn-default main-link disabled">Вернуться к центру карты</a></p>
+        <p class="mapcenter"><a class="btn btn-default main-link">Вернуться к центру карты</a></p>
     </div>
 </div>
 
 <!-- Опции карты -->
 <div class="optins_layerstability">
-    
-    <!-- todo: Убрать это после релиза -->
-    <div style="display:none;">
-        <div class="outer-button"><img src="/img/maps/button_outer.png"></div>
-        <div class="inner-button"><img src="/img/maps/button_inner.png"></div>
-    </div>
+
+    <div class="outer-button"><img src="/img/maps/button_outer.png"></div>
+    <div class="inner-button"><img src="/img/maps/button_inner.png"></div>
+
     
     
     <div class="col-lg-12">
         <div class="option-buttons">
-            <h2 class="map-title">Маркеры</h2>
-                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 map_buttons">
-                    <p class="dikie-b" id="dikie-spawns-bereg"><i class="dikie-spawns"></i>Спавны диких</p>
-                    <p class="gamers-b" id="chvk-spawns-bereg"><i class="chvk-spawns"></i>Спавны ЧВК</p>
-                    <p class="bandits-b" id="dikie-exits-bereg"><i class="dikie-exits"></i>Выходы с карты за Диких</p>
-                    <p class="exits-b" id="chvk-exits-bereg"><i class="chvk-exits"></i>Выходы с карты за ЧВК</p>
-                </div>
-            
-                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 map_buttons">
-                    <p class="voenka-b" id="weapons-bereg"><i class="weapon-cases"></i>Оружейные ящики</p>
-                    <p class="polki-b" id="quests-bereg"><i class="quest-tochki"></i>Квестовые точки</p>
-                    <p class="w-100 keys-b" id="doors-bereg"><i class="doors-tochki"></i>Открываемые двери</p>
-                    <p class="w-100 places-b" id="interesting-places-bereg"><i class="interest-tochki"></i>Интересные места</p>
-                </div>
+            <!-- todo: Включить, когда будет доступная официальная карта -->
+<!--            <h2 class="map-title">Маркеры</h2>-->
+<!--                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 map_buttons">-->
+<!--                    <p class="dikie-b" id="dikie-spawns-bereg"><i class="dikie-spawns"></i>Спавны диких</p>-->
+<!--                    <p class="gamers-b" id="chvk-spawns-bereg"><i class="chvk-spawns"></i>Спавны ЧВК</p>-->
+<!--                    <p class="bandits-b" id="dikie-exits-bereg"><i class="dikie-exits"></i>Выходы с карты за Диких</p>-->
+<!--                    <p class="exits-b" id="chvk-exits-bereg"><i class="chvk-exits"></i>Выходы с карты за ЧВК</p>-->
+<!--                </div>-->
+<!--            -->
+<!--                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 map_buttons">-->
+<!--                    <p class="voenka-b" id="weapons-bereg"><i class="weapon-cases"></i>Оружейные ящики</p>-->
+<!--                    <p class="polki-b" id="quests-bereg"><i class="quest-tochki"></i>Квестовые точки</p>-->
+<!--                    <p class="w-100 keys-b" id="doors-bereg"><i class="doors-tochki"></i>Открываемые двери</p>-->
+<!--                    <p class="w-100 places-b" id="interesting-places-bereg"><i class="interest-tochki"></i>Интересные места</p>-->
+<!--                </div>-->
 
             <!-- Функциональные кнопки --->
-                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 map_buttons">
-                    <p class="count-on" id="bereg-count-on"><i class="fa fa-check-square" aria-hidden="true"></i>Показать количество маркеров</p>
-                    <p class="markers-on">Показать все маркеры<i class="fa fa-eye"></i></p>
-                </div>
-
-                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 map_buttons">
-                    <p class="count-off" id="bereg-count-off"><i class="fa fa-square" aria-hidden="true"></i>Скрыть количество маркеров</p>
-                    <p class="markers-off"><i class="fa fa-eye-slash"></i>Скрыть все маркеры</p>
-                </div>
+<!--                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 map_buttons">-->
+<!--                    <p class="count-on" id="bereg-count-on"><i class="fa fa-check-square" aria-hidden="true"></i>Показать количество маркеров</p>-->
+<!--                    <p class="markers-on">Показать все маркеры<i class="fa fa-eye"></i></p>-->
+<!--                </div>-->
+<!---->
+<!--                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 map_buttons">-->
+<!--                    <p class="count-off" id="bereg-count-off"><i class="fa fa-square" aria-hidden="true"></i>Скрыть количество маркеров</p>-->
+<!--                    <p class="markers-off"><i class="fa fa-eye-slash"></i>Скрыть все маркеры</p>-->
+<!--                </div>-->
             
         </div>
         <!-- Контент страницы -->
@@ -110,8 +102,9 @@ $this->registerMetaTag([
                 <h2>Интерактивная карта Развязки</h2>
                 <p>Интерактивная карта локации Развязка из Escape from Tarkov - на данной карте, вы сможете увидеть спавны и выходы за диких и ЧВК с локации Развязка, узнать о местонахождении оружейных и военных ящиков. </p>
                 <p>Также с помощью интерактивной карты вы сможете узнать о местах спавна ключей от помещений и сейфов, которые спавнятся на карте Развязки. Интерактивная карта локации Развязка поможет вам очень быстро найти квестовые точки, на которых можно увидеть предметы, или места необходимые для выполнения квестов торговцев.</p>
+                <p>На данном варианте карты <b style="color:#c3392c;">маркеры реализованы не будут</b>, т.к. <b style="color:#c3392c;">этот вариант карты не является окончательным</b>.</p>
 
-                <p class="alert alert-danger"><b>В настоящий момент карта находится в разработке.</b></p>
+                <p class="alert alert-danger"><b>Это не окончательный вариант карты - когда будет доступна официальная версия игровой карты, данный вариант будет обновлен.</b></p>
             </div>
 
         </div>
--- web/js/razvyazka-location.js
+++ web/js/razvyazka-location.js
@@ -1,3 +1,115 @@
 /**
  * Created by comp on 05.04.2018.
  */
+
+/*** После релиза нормальной карты этот файл необходимо перезалить по аналогии с другими картами ***/
+
+/** Вызов карты и указание центра координат **/
+const map = L.map('map', {
+    center: [75.845, -68.906],
+    maxzoom: 6,
+    minzoom: 3,
+    zoom: 3,
+});
+
+/** Подключаем хэш в url для учета текущего зума и центра координат пользователя **/
+var hash = new L.Hash(map);
+
+/** Обращаемся к слоям зума интерактивной карты **/
+L.tileLayer('/img/razvyazka/{z}/{x}/{y}.png', {
+    noWrap: true,
+    errorTileUrl: '/img/error-bg.jpg',
+}).addTo(map);
+
+/** Устанавливаем зум карты на 2 также указываем что минимальный зум 2 а максимальный 4 **/
+map.setMaxZoom(6);
+map.setMinZoom(3);
+// map.setZoom(3);
+
+/** Получаем текщие координаты по местонахождению мышки **/
+function onMouseMove(e) {
+    $('#mapCoords').text((e.latlng.lat).toFixed(3) + ", " + (e.latlng.lng).toFixed(3));
+}
+map.on('mousemove', onMouseMove);
+
+$(document).ready(function() {
+
+    /** Делаем бэкграунд черным **/
+    $('body').css({'background':'black'});
+
+    /** Отключаем на минимальном зуме кнопку минуса **/
+    $('a.leaflet-control-zoom-out').addClass('leaflet-disabled');
+
+
+
+    /** Возвращаем пользователя к центру карты, если он кликнул на кнопку **/
+    $('body').on('click','.mapcenter', function(){
+        map.panTo(new L.LatLng(75.845, -72.906));
+    });
+
+    /** Инициализация OpenPopup **/
+    $('body').on('click','.leaflet-marker-icon', function() {
+        /** Указываем оборачивать все изображения в popup окнах классом JS Magnific - отлавливаем ошибки на несуществующие классы **/
+        try {
+            var MagnificImg = $('.leaflet-popup-content p img');
+            var MagnificTitle = MagnificImg.attr("alt").length > 0;
+        }
+
+        catch (error) {
+        }
+
+        if (MagnificTitle) {
+            $(MagnificImg).unwrap();
+            $(MagnificImg).wrap('<a class="image-link" title="' + $(MagnificImg).attr('alt') + '" href=' + $(MagnificImg).attr('src') + '></a>');
+        }
+
+        if (MagnificImg) {
+            $(MagnificImg).unwrap();
+            $(MagnificImg).wrap('<a class="image-link" title="' + $(MagnificImg).attr('alt') + '" href=' + $(MagnificImg).attr('src') + '></a>');
+        }
+
+        /** Инициализация самого скрипта **/
+        $('.image-link').magnificPopup(
+            {
+                type: 'image',
+                showCloseBtn: true,
+                mainClass: 'image-link'
+            });
+
+        $('.parent-container').each(function () { // the containers for all your galleries
+            $(this).magnificPopup({
+                delegate: 'a',  // the selector for gallery item
+                type: 'image',
+                gallery: {
+                    enabled: true
+                }
+            });
+        });
+    });
+
+
+    /** Убираем и показываем боковое меню при клике на стрелочки а также проверки разрешения окна браузера клиента **/
+    $.wait = function( callback, seconds){
+        return window.setTimeout(callback, seconds * 800 );
+    };
+
+    $('.outer-button').click(function () {
+        $(".optins_layerstability").animate({ right: -540}, 800);
+        $.wait(function(){$(".outer-button").hide()} ,1);
+        $.wait(function(){$(".inner-button").show()} ,1);
+    });
+
+    $('.inner-button').click(function () {
+        $(".optins_layerstability").animate({ right: 0 }, 800);
+        $.wait(function(){$(".inner-button").hide()} ,1);
+        $.wait(function(){$(".outer-button").show()} ,1);
+    });
+
+    if (document.body.clientWidth <= '768')  {
+        $('.outer-button').click(function () {
+            $(".optins_layerstability").animate({ right: -327}, 800);
+            $.wait(function(){$(".outer-button").hide()} ,1);
+            $.wait(function(){$(".inner-button").show()} ,1);
+        });
+    }
+});
