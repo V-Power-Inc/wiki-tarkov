0819331d2a59a9e821ea0c6c4e9593dc3cb46633
Василий <basil2305@ya.ru>
2018-07-14 01:39:28 +0400

    14-07-2018 - Запилил предпросмотр материалов торговцев

---
+++ web/js/preview-traders.js
@@ -0,0 +1,28 @@
+/** Файл позволяет смотреть превью страницы торговца перед публикацией **/
+$(document).ready(function() {
+
+    /*** Собираем данные в скрытую форму и отправляем их в нужный экшон ***/
+    $('#preview-print').click(function(){
+
+        var title = $('#traders-title').val();
+        var preview = $('#traders-file').attr("value");
+        var content = $('#traders-fullcontent').val();
+        var url = $('#traders-urltoquets').val();
+        var buttonquests = $('#traders-button_quests').val();
+        var buttondetail = $('#traders-button_detail').val();
+
+
+
+        //
+        $('#text-title').val(title);
+        $('#text-preview').val(preview);
+        $('#text-url').val(url);
+        $('#text-button').val(buttonquests);
+        $('#text-button-detail').val(buttondetail);
+        $('#text-fullcontent').val(content);
+
+
+        var form = document.getElementById("prev-form");
+        form.submit();
+    });
+});
