40fb24da6aaa33d88e099afeadb04daffd8bc96e
DIR300NRU-ADMIN (Toshiba) <basil2305@ya.ru>
2018-09-20 10:27:28 +0300

    20-09-2018 - Отключили рекламу на запрещенных страницах Google Adsense

--- views/other/yandex-direct.php
+++ views/other/yandex-direct.php
@@ -9,7 +9,7 @@
 ?>
 
 
-<?php if(Yii::$app->request->url !== '/loot/modules/sight' && Yii::$app->request->url !== '/loot/telescopic-sight-hamr-deltapoint.html'): ?>
+<?php if(Yii::$app->request->url !== '/loot/modules/sight' && Yii::$app->request->url !== '/loot/telescopic-sight-hamr-deltapoint.html' && Yii::$app->request->url !== '/loot/weapons/rifles'): ?>
 
 <div class="no-adb">
     <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
