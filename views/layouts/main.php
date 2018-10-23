543f87e8ac148ebb4de670148e94b56c54b07b1d
DIR300NRU-ADMIN (Toshiba) <basil2305@ya.ru>
2018-10-23 13:29:20 +0300

    23-10-2018 -  Заменили гугл адсенс на свой собственный аккаунт

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -17,14 +17,13 @@ AppAsset::register($this);
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
 <head>
-    
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-125987040-1"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
 
-        gtag('config', 'UA-125987040-1');
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({
+            google_ad_client: "ca-pub-9672363886013669",
+            enable_page_level_ads: true
+        });
     </script>
 
     <meta charset="<?= Yii::$app->charset ?>">
