f9d9472b941e6c77f0e232a4b29d0e1af8028abc
DIR300NRU-ADMIN (Toshiba) <basil2305@ya.ru>
2018-10-29 14:53:31 +0300

    29-10-2018 -  Вернули блоки рекомендуемого контента

--- views/other/google-recommended.php
+++ views/other/google-recommended.php
@@ -9,8 +9,15 @@
 
 
 <?php if(Yii::$app->request->url !== '/loot/modules/sight' && Yii::$app->request->url !== '/loot/telescopic-sight-hamr-deltapoint.html'): ?>
-
-<!-- google recommended block -->
-
+    
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <ins class="adsbygoogle"
+         style="display:block"
+         data-ad-format="autorelaxed"
+         data-ad-client="ca-pub-9672363886013669"
+         data-ad-slot="3148878182"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
     
 <?php endif; ?>
