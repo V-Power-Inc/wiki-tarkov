bde9ac0bb68fda8cbafe638578569a0cc48b98be
DIR300NRU-ADMIN (Toshiba) <basil2305@ya.ru>
2018-11-01 12:37:22 +0300

    01-11-2018 -  Реклама Google adsense была восстановлена

--- views/other/google-gor.php
+++ views/other/google-gor.php
@@ -12,9 +12,17 @@
 
 <div class="no-adb">
 
-
-
-
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <ins class="adsbygoogle"
+         style="display:block"
+         data-ad-client="ca-pub-5071904663034434"
+         data-ad-slot="7149710678"
+         data-ad-format="auto"
+         data-full-width-responsive="true"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+    
 </div>
 
 <?php endif; ?>
