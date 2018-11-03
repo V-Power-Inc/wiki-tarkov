309bf16f1c69b8bf0e7e5e1b500c8f79e761f9ed
Василий <basil2305@ya.ru>
2018-11-03 20:00:35 +0400

    03-11-2018 - Peremestili kommenty kyda nado (skills detail pages)

--- views/skills/skillscat-page.php
+++ views/skills/skillscat-page.php
@@ -127,7 +127,27 @@ use app\components\AlertComponent;
             <div class="recommended-gm-content">
                 <?= $this->render('/other/google-recommended.php'); ?>
             </div>
-            
+
+            <!-- Расстояние - заглушка -->
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 height-25"></div>
+
+            <!-- Комментарии -->
+            <?php if(empty($_GET)) : ?>
+                <div id="mc-container" class="kek-recustom"></div>
+                <script type="text/javascript">
+                    cackle_widget = window.cackle_widget || [];
+                    cackle_widget.push({widget: 'Comment', id: 57165});
+                    (function() {
+                        var mc = document.createElement('script');
+                        mc.type = 'text/javascript';
+                        mc.async = true;
+                        mc.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://cackle.me/widget.js';
+                        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(mc, s.nextSibling);
+                    })();
+                </script>
+            <?php endif; ?>
+
+
         </div>
 
 
@@ -150,26 +170,7 @@ use app\components\AlertComponent;
             <?= $this->render('/other/yandex-direct.php'); ?>
 
         </div>
-        
 
-        <!-- Расстояние - заглушка -->
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 height-25"></div>
-
-        <!-- Комментарии -->
-        <?php if(empty($_GET)) : ?>
-            <div id="mc-container" class="kek-recustom"></div>
-            <script type="text/javascript">
-                cackle_widget = window.cackle_widget || [];
-                cackle_widget.push({widget: 'Comment', id: 57165});
-                (function() {
-                    var mc = document.createElement('script');
-                    mc.type = 'text/javascript';
-                    mc.async = true;
-                    mc.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://cackle.me/widget.js';
-                    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(mc, s.nextSibling);
-                })();
-            </script>
-        <?php endif; ?>
 
     </div>
 </div>
