9c65a59050fc4062976bd730f6e4fb6d8e91fcd8
DIR300NRU-ADMIN (Toshiba) <basil2305@ya.ru>
2018-12-19 16:40:55 +0300

    19-12-2018 - Ссылаемся на наш магазин с футболками

--- views/other/yandex-direct.php
+++ views/other/yandex-direct.php
@@ -20,16 +20,23 @@
 
 <div class="no-adb fortunite-block">
 
-    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <ins class="adsbygoogle"
-         style="display:block"
-         data-ad-client="ca-pub-5071904663034434"
-         data-ad-slot="1963713047"
-         data-ad-format="auto"
-         data-full-width-responsive="true"></ins>
-    <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
+    <a href="https://v-power.myprintbar.ru/?referrals=v-power&reffer=tarkovwiki" onclick="yaCounter47100633.reachGoal('shop-link'); return true;" target="_blank">
+        <img class="mx-100" src="/img/banner-prt.jpg">
+    </a>
+
+
+
+
+<!--    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>-->
+<!--    <ins class="adsbygoogle"-->
+<!--         style="display:block"-->
+<!--         data-ad-client="ca-pub-5071904663034434"-->
+<!--         data-ad-slot="1963713047"-->
+<!--         data-ad-format="auto"-->
+<!--         data-full-width-responsive="true"></ins>-->
+<!--    <script>-->
+<!--        (adsbygoogle = window.adsbygoogle || []).push({});-->
+<!--    </script>-->
 
 </div>
 
