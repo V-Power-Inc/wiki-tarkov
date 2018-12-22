3c4d7d5d0fafd5c897b03ba9b16d755a0ae18950
Василий <basil2305@ya.ru>
2018-12-22 21:46:26 +0400

    22-12-2018 - Реклама была внедрена на страницы частых ответов и вопросов

---
+++ views/other/adsense-questions-feed.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: comp
+ * Date: 22.12.2018
+ * Time: 20:42
+ */
+
+/*** Эта вьюха выводит рекламные фиды в списке частых вопросов - сделана отдельной т.к. не совпадает по стилю с остальными разделами сайта ***/
+?>
+
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-format="fluid"
+     data-ad-layout-key="-h7-22+2o-1c+81"
+     data-ad-client="ca-pub-5071904663034434"
+     data-ad-slot="7893572786"></ins>
+<script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
