80a75b3789046dec838acd49488db2ed06926432
Василий <basil2305@ya.ru>
2019-01-06 02:28:24 +0400

    06-01-2018 - Подправили мини баг с рекламой

--- web/js/conv.js
+++ web/js/conv.js
@@ -16,13 +16,13 @@ $(document).ready(function() {
 
         if(Cwidth > 1199 && Cheight >= 720) {
             if(addr.includes(loot)) {
-                if ($(window).scrollTop() >= 3022) {
+                if ($(window).scrollTop() >= 2466) {
                     $('.fortunite-block').css({'position': 'fixed', 'top': '70px'});
                 } else {
                     $('.fortunite-block').css({'position': 'unset', 'top': 'initial'});
                 }
             } else if (addr.includes(skills)) {
-                if ($(window).scrollTop() >= 2622) {
+                if ($(window).scrollTop() >= 2066) {
                     $('.fortunite-block').css({'position': 'fixed', 'top': '70px'});
                 } else {
                     $('.fortunite-block').css({'position': 'unset', 'top': 'initial'});
