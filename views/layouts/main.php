37334996636e742a9f61d9e93b021eaf0dd3cf4f
DIR300NRU-ADMIN (Toshiba) <basil2305@ya.ru>
2019-01-16 13:06:06 +0300

    16-01-2019 - Verification MiraLinks

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -52,6 +52,8 @@ AppAsset::register($this);
         (function(){ var widget_id = 'OXowZeYTop';var d=document;var w=window;function l(){
             var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);}if(d.readyState=='complete'){l();}else{if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();</script>
     <!-- {/literal} END JIVOSITE CODE -->
+
+    <meta name="442ee7acba0cd76db400fc3fe05d5327" content="">
 </head>
 
 <body>
