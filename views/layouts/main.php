feae7243219cd64ae72702fbf173a87c2cbca540
Vlad <enslaver45@gmail.com>
2019-01-20 14:44:16 -0600

    20-01-2019 - Вовчик в деле

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -30,6 +30,7 @@ AppAsset::register($this);
         });
     </script>
 
+
     <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
