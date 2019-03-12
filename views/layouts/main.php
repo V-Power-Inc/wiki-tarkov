74864bfd5d8b781f4c31169e9f020d1bafd25beb
DIR300NRU-ADMIN (Toshiba) <basil2305@ya.ru>
2019-03-12 10:17:14 +0300

    12-03-2019 - Исключили страницу с добавлением клана на проверку присутстствия включенного Adblock

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -104,7 +104,7 @@ AppAsset::register($this);
 
 
 <?php if($status_response !== 404): ?>
-    <?php if(Yii::$app->request->url !== '/loot/modules/sight' && Yii::$app->request->url !== '/loot/telescopic-sight-hamr-deltapoint.html' && Yii::$app->request->url !== '/loot/weapons/rifles' && Yii::$app->request->url !== '/loot/weapons/pistols' && Yii::$app->request->url !== '/loot/modules/pistol-grip' && !stristr(Yii::$app->request->url,'/loot/modules?page') && Yii::$app->request->url !== '/loot/modules/trunk' && Yii::$app->request->url !== '/site/barters-preview' && Yii::$app->request->url !== '/loot/quest-loot' && Yii::$app->request->url !== '/item/preview' && Yii::$app->request->url !== '/loot/modules'): ?>
+    <?php if(Yii::$app->request->url !== '/loot/modules/sight' && Yii::$app->request->url !== '/loot/telescopic-sight-hamr-deltapoint.html' && Yii::$app->request->url !== '/loot/weapons/rifles' && Yii::$app->request->url !== '/loot/weapons/pistols' && Yii::$app->request->url !== '/loot/modules/pistol-grip' && !stristr(Yii::$app->request->url,'/loot/modules?page') && Yii::$app->request->url !== '/loot/modules/trunk' && Yii::$app->request->url !== '/site/barters-preview' && Yii::$app->request->url !== '/loot/quest-loot' && Yii::$app->request->url !== '/item/preview' && Yii::$app->request->url !== '/loot/modules' && Yii::$app->request->url !== '/add-clan'): ?>
         <script type="text/javascript" src="/js/core-checker.js"></script>
     <?php endif; ?>
 <?php endif; ?>
