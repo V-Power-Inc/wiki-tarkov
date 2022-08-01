43320e68feecabdecc7efcebd143f37c291e1caa
PC_Principal <basil2305@ya.ru>
2022-08-01 21:56:42 +0300

    01-08-2022 - РСЯ рекламный блок также добавлен на главную страницу сайта

---
+++ views/other/yandex-direct-mainpage.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: basil
+ * Date: 01.08.2022
+ * Time: 21:54
+ *
+ * Рекламный блок на главной странице сайта
+ */
+?>
+
+<div class="rl_cnt_bg" data-id="298950"></div>
