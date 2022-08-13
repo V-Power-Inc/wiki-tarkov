b0a8f8f213bb0abc79ef924b7102ccb4b2852ddc
PC_Principal <basil2305@ya.ru>
2022-08-13 21:53:05 +0300

    13-08-2022 - Указали в явном виде в конфиге, без каких переменных .env приложение не стартанет (Required validation)

--- common/env.php
+++ common/env.php
@@ -5,12 +5,26 @@
  * Date: 07.08.2022
  * Time: 18:21
  * @repo https://github.com/vlucas/phpdotenv
- * todo: Добавить в файл Required проверки необходмых атрибутов
  */
 
 /**
  * Load application environment from .env file
  */
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+
+
 $dotenv->load();
+
+/**
+ * Эти глобальные переменные должны быть определены в .env, иначе будет 500-сотить
+ */
+$dotenv->required([
+    'DB_DSN',
+    'DB_NAME',
+    'DB_USER',
+    'DB_PASSWORD',
+    'DB_CHARSET',
+    'DOMAIN_PROTOCOL',
+    'DOMAIN'
+]);
 ?>
