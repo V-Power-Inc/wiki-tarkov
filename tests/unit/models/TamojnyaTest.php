1d4e6cab3a1e660a421676cec0b594eeab047a64
PC_Principal <basil2305@ya.ru>
2022-08-19 23:31:48 +0300

    19-08-2022 - Unit тесты для таблицы Развязка в деле

--- tests/unit/models/TamojnyaTest.php
+++ tests/unit/models/TamojnyaTest.php
@@ -34,69 +34,69 @@ class TamojnyaTest extends \Codeception\Test\Unit
     /** Тестируем создание нового маркера */
     public function testCreate()
     {
-        $zavod = new Zavod();
+        $tamojnya = new Zavod();
 
-        $zavod->name = 'Ящик у выхода с локации';
-        $zavod->marker_group='Военные ящики';
-        $zavod->coords_x='10';
-        $zavod->coords_y='-135';
-        $zavod->content='<p>Это основной выход с локации Завод, если у вас нет ключа от выхода с завода - это ваш единственный способ выйти с карты.</p><p><img alt="Основной выход с локации Завод. Через него выходит основная часть игроков." src="/img/upload/bereg_images/vihod-s-karty.png" style="width:100%" /></p>';
-        $zavod->enabled='1';
-        $zavod->customicon='/img/admin/beregicons/vorota_3_d.png';
-        $zavod->exits_group='Спавн на зеленой лампе';
-        $zavod->exit_anyway='1';
+        $tamojnya->name = 'Ящик у выхода с локации';
+        $tamojnya->marker_group='Военные ящики';
+        $tamojnya->coords_x='10';
+        $tamojnya->coords_y='-135';
+        $tamojnya->content='<p>Это основной выход с локации Завод, если у вас нет ключа от выхода с завода - это ваш единственный способ выйти с карты.</p><p><img alt="Основной выход с локации Завод. Через него выходит основная часть игроков." src="/img/upload/bereg_images/vihod-s-karty.png" style="width:100%" /></p>';
+        $tamojnya->enabled='1';
+        $tamojnya->customicon='/img/admin/beregicons/vorota_3_d.png';
+        $tamojnya->exits_group='Спавн на зеленой лампе';
+        $tamojnya->exit_anyway='1';
 
-        $this->assertTrue($zavod->save(), 'Ожидалось true, вернулось false - объект не сохранился.');
+        $this->assertTrue($tamojnya->save(), 'Ожидалось true, вернулось false - объект не сохранился.');
     }
 
     /** Тестируем обновление маркера */
     public function testUpdate()
     {
-        $zavod = Zavod::find()->where(['is not','id',null])->one();
+        $tamojnya = Zavod::find()->where(['is not','id',null])->one();
 
-        $zavod->name = 'Ящик у выхода 234234с локации';
-        $zavod->marker_group='Военн234234ые ящики';
-        $zavod->coords_x='10';
-        $zavod->coords_y='-135';
-        $zavod->content='<p>Это основной выхо34234234д с локации Завод, если у вас нет ключа от выхода с завода - это ваш единственный способ выйти с карты.</p><p><img alt="Основной выход с локации Завод. Через него выходит основная часть игроков." src="/img/upload/bereg_images/vihod-s-karty.png" style="width:100%" /></p>';
-        $zavod->enabled='0';
-        $zavod->customicon='/img/orota_3_d.png';
-        $zavod->exits_group='Другое значение';
-        $zavod->exit_anyway='0';
+        $tamojnya->name = 'Ящик у выхода 234234с локации';
+        $tamojnya->marker_group='Военн234234ые ящики';
+        $tamojnya->coords_x='10';
+        $tamojnya->coords_y='-135';
+        $tamojnya->content='<p>Это основной выхо34234234д с локации Завод, если у вас нет ключа от выхода с завода - это ваш единственный способ выйти с карты.</p><p><img alt="Основной выход с локации Завод. Через него выходит основная часть игроков." src="/img/upload/bereg_images/vihod-s-karty.png" style="width:100%" /></p>';
+        $tamojnya->enabled='0';
+        $tamojnya->customicon='/img/orota_3_d.png';
+        $tamojnya->exits_group='Другое значение';
+        $tamojnya->exit_anyway='0';
 
-        $this->assertIsInt($zavod->update(), 'Ожидался int, вернулся false - объект не удалился.');
+        $this->assertIsInt($tamojnya->update(), 'Ожидался int, вернулся false - объект не удалился.');
     }
 
     /** Тестируем получение объекта (select) */
     public function testSelect()
     {
-        $zavod = Zavod::find()->one();
+        $tamojnya = Zavod::find()->one();
 
-        $this->assertNotNull($zavod, 'Ожидался объект, вернулся null - объект не селектнулся.');
+        $this->assertNotNull($tamojnya, 'Ожидался объект, вернулся null - объект не селектнулся.');
     }
 
     /** Тестируем получение всех объектов (select all) */
     public function testSelectAll()
     {
-        $zavod = Zavod::find()->all();
+        $tamojnya = Zavod::find()->all();
 
-        $this->assertTrue(count($zavod) == 1, 'Ожидалось что вернется 3 объекта, что то пошло не так');
+        $this->assertTrue(count($tamojnya) == 1, 'Ожидалось что вернется 3 объекта, что то пошло не так');
     }
 
     /** Тестируем удаление объекта */
     public function testDelete()
     {
-        $zavod = Zavod::find()->one()->delete();
+        $tamojnya = Zavod::find()->one()->delete();
 
-        $this->assertIsInt($zavod,'Удаление объекта не случилось, а должно было.');
+        $this->assertIsInt($tamojnya,'Удаление объекта не случилось, а должно было.');
     }
 
     /** Тестируем удаление всех объектов */
     public function testDeleteAll()
     {
-        $zavod = Zavod::deleteAll();
+        $tamojnya = Zavod::deleteAll();
 
-        $this->assertIsInt($zavod,'Удаление объекта не случилось, а должно было.');
+        $this->assertIsInt($tamojnya,'Удаление объекта не случилось, а должно было.');
     }
 
 }
