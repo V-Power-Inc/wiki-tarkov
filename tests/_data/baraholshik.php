65bc8e2958d7faef2553020d4ea85f1f7c5d8555
PC_Principal <basil2305@ya.ru>
2022-08-23 09:20:37 +0300

    23-08-2022 - Зарядили Unit тесты для Барахольщика (Остался Механик)

---
+++ tests/_data/baraholshik.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm
+ * User: PC_Principal
+ * Date: 23.08.2022
+ * Time: 9:10
+ *
+ * Фикстуры для торговца Барахольщик
+ */
+return [
+    [
+        'tab_number' => 1,
+        'title' => 'Забудем старые обиды',
+        'content' => '<ol><li><span style="font-size:16px">Найти ключ от 303 комнаты общежития</span></li><li><span style="font-size:16px">Найти ключ ЗБ-014</span></li><li><span style="font-size:16px">Найти ключ от КПП военной базы на Таможне</span></li><li><span style="font-size:16px">Найти ключ склада на заправке</span></li><li><span style="font-size:16px">Передайте ключи Терапевту&nbsp;</span></li></ol>',
+        'preview' => '/img/admin/resized/prostoe-lyubopitstvo030318045933.jpg'
+    ],
+    [
+        'tab_number' => 2,
+        'title' => 'Второй квест',
+        'content' => '<ol><li><span style="font-size:16px">Найти ключ от 303 комнаты общежития</span></li><li><span style="font-size:16px">Найти ключ ЗБ-014</span></li><li><span style="font-size:16px">Найти ключ от КПП военной базы на Таможне</span></li><li><span style="font-size:16px">Найти ключ склада на заправке</span></li><li><span style="font-size:16px">Передайте ключи Терапевту&nbsp;</span></li></ol>',
+        'preview' => '/img/admin/resized/image030318045933.jpg'
+    ],
+    [
+        'tab_number' => 3,
+        'title' => 'Третий квест',
+        'content' => '<ol><li><span style="font-size:16px">Найти ключ от 423 комнаты общежития</span></li><li><span style="font-size:16px">Найти ключ ЗБ-014</span></li><li><span style="font-size:16px">Найти ключ от КПП военной базы на Таможне</span></li><li><span style="font-size:16px">Найти ключ склада на заправке</span></li><li><span style="font-size:16px">Передайте ключи Терапевту&nbsp;</span></li></ol>',
+        'preview' => '/img/admin/resized/thread344318045933.jpg'
+    ]
+];
---
+++ tests/fixtures/BaraholshikFixture.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\tests\fixtures;
+
+class BaraholshikFixture extends \yii\test\ActiveFixture
+{
+
+    public $modelClass = 'app\models\Baraholshik';
+
+    /** @var string Имя таблицы */
+    const TABLE_NAME = 'baraholshik';
+
+//    Действие для фикстур после отработки каждого теста
+//    public function afterLoad() {
+//        parent::afterLoad();
+//        $this->db->createCommand()->setSql('truncate table .'.static::TABLE_NAME)->execute();
+//    }
+}
