73eae302f48466b8eb3335a903198a689b58e8bc
PC_Principal <basil2305@ya.ru>
2022-08-25 13:51:22 +0300

    25-08-2022 - Реализовали функциональные тесты для страницы торговца Прапор (Вечером проверить, если все ок - сделать аналогичный подход и для других торговцев

--- tests/functional/PraporpageCest.php
+++ tests/functional/PraporpageCest.php
@@ -1,34 +1,34 @@
 <?php
 /**
- * Created by PhpStorm.
+ * Created by PhpStorm
  * User: PC_Principal
- * Date: 24.08.2022
- * Time: 21:11
+ * Date: 25.08.2022
+ * Time: 13:23
  */
 
 namespace Tests\Functional;
 
-use app\controllers\SiteController;
+use app\controllers\TraderController;
 
 /**
- * Функциональные тесты главной страницы сайта
+ * Функциональные тесты страницы торговца Прапор
  *
- * Class MainpageCest
+ * Class PraporpageCest
  * @package Tests\Functional
  */
-class MainpageCest
+class PraporpageCest
 {
-    /** Мы на главной странице */
+    /** Мы на странице Прапора */
     public function _before(\FunctionalTester $I)
     {
-        $I->amOnRoute(SiteController::routeId(SiteController::ACTION_INDEX));
+        $I->amOnRoute(TraderController::routeId(TraderController::ACTION_PRAPORPAGE));
     }
 
     /** Мы видим что все метатеги в head присутствуют и соответствуют нашим стандартам */
     public function checkMetaTagsData(\FunctionalTester $I)
     {
-        $I->seeInSource('<meta name="description" content="Интерактивные карты локаций, описания квестов торговцев и их прохождения, карта ключей от помещений.">');
-        $I->seeInSource('<meta name="keywords" content="Интерактивные карты локаций Escape from Tarkov, квесты Escape from Tarkov, Ключи Escape from Tarkov.">');
+        $I->seeInSource('<meta name="description" content="Прапор. Escape from Tarkov.">');
+        $I->seeInSource('<meta name="keywords" content="Прапор из Escape from Tarkov, Что продаёт Прапор в Escape from Tarkov, все о торговце Прапоре.">');
     }
 
     /** Мы видим что все OpenGraph теги соответствуют нашим стандартам */
@@ -36,21 +36,14 @@ class MainpageCest
     {
         $I->seeInSource('<meta property="og:type" content="website">');
         $I->seeInSource('<meta property="og:site_name" content="База знаний Escape from Tarkov">');
-        $I->seeInSource('<meta property="og:title" content="База знаний Escape from Tarkov. Карты локаций, ключи от дверей, разбор квестов торговцев">');
+        $I->seeInSource('<meta property="og:title" content="Торговцы Escape from Tarkov: Прапор">');
         $I->seeInSource('<meta property="og:image" content="/img/logo-full.png">');
     }
 
     /** Мы видим корректный Title */
     public function checkTitle(\FunctionalTester $I)
     {
-        $I->seeInTitle('База знаний Escape from Tarkov. Карты локаций, ключи от дверей, разбор квестов торговцев');
-    }
-
-    /** Мы видим H1 заголовок и кнопку перейти к интерактивным картам */
-    public function checkPageMainData(\FunctionalTester $I)
-    {
-        $I->see('База знаний Escape from Tarkov', 'h1');
-        $I->see('Перейти к интерактивным картам Escape from Tarkov');
+        $I->seeInTitle('Торговцы Escape from Tarkov: Прапор');
     }
 
     /** Мы видим все ссылки горизонтального меню */
@@ -89,6 +82,29 @@ class MainpageCest
         $I->seeLink('Справочник ключей','/keys');
     }
 
+    /** Проверяем активность пункта навигации, для текущей страницы */
+    public function checkCurrentPageLinkActive(\FunctionalTester $I)
+    {
+        $I->seeLink('<b>Торговцы</b><span class="caret"></span>', '#');
+    }
+
+    /**
+     * Мы видим H1 заголовок и основные элементы данной страницы такие как
+     * Заголовок, детальное содержмое страницы, наличие табов, наличие ключевых ссылок на
+     * релевантные разделы
+     */
+    public function checkPageMainData(\FunctionalTester $I)
+    {
+        $I->see('Торговцы Escape from Tarkov: Прапор', 'h1');
+
+        $I->seeElement('.news-shortitem.bg-white');
+        $I->seeElement('.barters-block');
+        $I->seeElement('.tab-content');
+
+        $I->seeLink('Вернуться к списку торговцев','/quests-of-traders');
+        $I->seeLink('Перейти в раздел квестов Прапора','/quests-of-traders/prapor-quests');
+    }
+
     /** У нас нет куки - скрывающей оверлей с рекламой */
     public function checkAdsCoockie(\FunctionalTester $I)
     {
@@ -107,9 +123,4 @@ class MainpageCest
         $I->seeElement('.no-adb');
     }
 
-    /** Мы видим что все 3 рекламных блока на месте */
-    public function checkCountOfAdsBlocks(\FunctionalTester $I)
-    {
-        $I->seeNumberOfElements('.no-adb', 3);
-    }
 }
