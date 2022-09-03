361cd6121f7ad6e014eababd002576c9203ac4d4
PC_Principal <basil2305@ya.ru>
2022-09-03 18:42:44 +0300

    03-09-2022 - Подвезли функциональное тестирование квестов Механика

--- tests/_data/mehanic.php
+++ tests/_data/mehanic.php
@@ -10,19 +10,19 @@
 return [
     [
         'tab_number' => 1,
-        'title' => 'Забудем старые обиды',
+        'title' => 'Оружейник. Часть 1',
         'content' => '<ol><li><span style="font-size:16px">Найти ключ от 303 комнаты общежития</span></li><li><span style="font-size:16px">Найти ключ ЗБ-014</span></li><li><span style="font-size:16px">Найти ключ от КПП военной базы на Таможне</span></li><li><span style="font-size:16px">Найти ключ склада на заправке</span></li><li><span style="font-size:16px">Передайте ключи Терапевту&nbsp;</span></li></ol>',
         'preview' => '/img/admin/resized/prostoe-lyubopitstvo030318045933.jpg'
     ],
     [
         'tab_number' => 2,
-        'title' => 'Второй квест',
+        'title' => 'Оружейник. Часть 2',
         'content' => '<ol><li><span style="font-size:16px">Найти ключ от 303 комнаты общежития</span></li><li><span style="font-size:16px">Найти ключ ЗБ-014</span></li><li><span style="font-size:16px">Найти ключ от КПП военной базы на Таможне</span></li><li><span style="font-size:16px">Найти ключ склада на заправке</span></li><li><span style="font-size:16px">Передайте ключи Терапевту&nbsp;</span></li></ol>',
         'preview' => '/img/admin/resized/image030318045933.jpg'
     ],
     [
         'tab_number' => 3,
-        'title' => 'Третий квест',
+        'title' => 'Оружейник. Часть 3',
         'content' => '<ol><li><span style="font-size:16px">Найти ключ от 423 комнаты общежития</span></li><li><span style="font-size:16px">Найти ключ ЗБ-014</span></li><li><span style="font-size:16px">Найти ключ от КПП военной базы на Таможне</span></li><li><span style="font-size:16px">Найти ключ склада на заправке</span></li><li><span style="font-size:16px">Передайте ключи Терапевту&nbsp;</span></li></ol>',
         'preview' => '/img/admin/resized/thread344318045933.jpg'
     ]
