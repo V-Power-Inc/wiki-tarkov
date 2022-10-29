5c2d892b0765dfc8856a39551facf5d8ad8be1d7
root <root@wiki-tarkov.ru>
2022-10-29 16:19:54 +0300

    Fix: На PROD подстановка переменной окружения DEBUG

--- web/index.php
+++ web/index.php
@@ -1,7 +1,7 @@
 <?php
 
 // comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true;
+defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', $_ENV['ENVIRONMENT']);
 
 require __DIR__ . '/../vendor/autoload.php';
