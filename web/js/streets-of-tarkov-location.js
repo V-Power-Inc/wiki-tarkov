816c16d1242bdc6768c6ab76e7ad20634c65e0a8
PC_Principal <basil2305@ya.ru>
2023-01-04 16:54:43 +0300

    Рефакторинг интерактивной карты "Улицы Таркова"

--- web/js/streets-of-tarkov-location.js
+++ web/js/streets-of-tarkov-location.js
@@ -1,47 +1,175 @@
-// todo: На примере этой карты будет проведен рефакторинг
+/**
+ * JS Обработчик карты - улицы Таркова
+ *
+ * @link https://mourner.github.io/Leaflet/reference.html - Documentation LeafletJS
+ */
 
-/** Вызов карты и указание центра координат **/
-const map = L.map('map', {
+/** Константа с опциями карты **/
+const Map = L.map('map', {
+
+    /** Положение центра карты */
     center: [67, -5],
+
+    /** Максимально возможный зум карты */
     maxzoom: 4,
+
+    /** Минимально возможный зум карты */
     minzoom: 1,
+
+    /** Дефолтный зум карты */
     zoom: 1
 });
 
-/** Обращаемся к слоям зума интерактивной карты **/
-L.tileLayer('/img/streets-of-tarkov/{z}/{x}/{y}.webp', {
-    noWrap: true,
-}).addTo(map);
+/** Константа с набором иконок для различных маркеров */
+const Icons = {
+
+    /** Иконки Диких */
+    ScawsIcon: L.icon({
+        iconUrl: '/img/mapicons/dikie.png',
+        iconSize: [30, 30]
+    }),
+
+    /** Иконки ЧВК */
+    ChvkIcon: L.icon({
+        iconUrl: '/img/mapicons/chvk.png',
+        iconSize: [30, 30]
+    })
+};
+
+/** Константа с набором слоев для карты (Группы маркеров) */
+const Layers = {
+
+    /** Слой маркеров Диких */
+    Scaws: L.layerGroup(),
+
+    /** Слой маркеров спавнов ЧВК */
+    Chvk: L.layerGroup()
+};
+
+/** Добавляем базовый слой интерактивной карты (С опцией - без повторения карты) **/
+let baseTileLayer = L.tileLayer('/img/streets-of-tarkov/{z}/{x}/{y}.webp', {noWrap: true}).addTo(Map);
+
+/** Добавляем чекбокс слоя с Дикими */
+let ScawsControl = L.Control.extend({
+
+    /** Опции чекбокса (Положение на карте) */
+    options: {
+        position: 'topright'
+    },
+
+    /** Обработчик события при добавлении на карту */
+    onAdd: function () {
+
+        /** Создание необходимых HTML блоков */
+        let div = L.DomUtil.create('div', 'scaws-c');
+
+        /** Создание необходимых HTML блоков */
+        div.innerHTML = '<div class="form-control"><input id="ids-control" class="scaws-control" type="checkbox"/>Спавны Диких</div>';
+        return div;
+    }
+});
+
+/** Добавляем чекбокс слоя с ЧВК */
+let ChvkControl = L.Control.extend({
+
+    /** Опции чекбокса (Положение на карте) */
+    options: {
+        position: 'topright'
+    },
+
+    /** Обработчик события при добавлении на карту */
+    onAdd: function () {
+
+        /** Создание необходимых HTML блоков */
+        let div = L.DomUtil.create('div', 'chvk-c');
+
+        /** Создание необходимых HTML блоков */
+        div.innerHTML = '<div class="form-control"><input id="ids-control" class="chvk-control" type="checkbox"/>Спавн ЧВК</div>';
+        return div;
+    }
+});
+
+/** Добавляем на карту интерактивные элементы */
+Map.addControl(new ScawsControl());
+Map.addControl(new ChvkControl());
+
+/** Функция обработчика чекбокса со спавнами диких (Параметр сюда должен прилететь this */
+function handleControl(Data) {
+
+    /** В свитче отлавливаем класс чекбокса и в зависимости от состояния слоев, показываем их или нет */
+    switch (Data.className) {
+        case 'scaws-control':
+            if (Data.checked === true) {
+                Map.addLayer(Layers.Scaws)
+            } else {
+                Map.removeLayer(Layers.Scaws)
+            }
+            break;
+        case 'chvk-control': {
+            if (Data.checked === true) {
+                Map.addLayer(Layers.Chvk)
+            } else {
+                Map.removeLayer(Layers.Chvk)
+            }
+            break;
+        }
+    }
+}
+
+/** Добавляем слушатель событий изменения чекбоксов Leaflet */
+$('body').on('click', '#ids-control', function() {
+
+    /** Вызываем обработчик события и передаем в него this - для дальнейшей работы */
+    handleControl(this);
+});
+
+/** Добавляем в слои карты Маркеры (Потом это можно будет сделать нормально - когда данные в бекенд положим (Switch например */
+L.marker([43.299, -18.457], {icon: Icons.ScawsIcon}).bindPopup('Спавн диких').openPopup().addTo(Layers.Scaws);
+L.marker([28.179, -70.664], {icon: Icons.ScawsIcon}).bindPopup('Спавн диких').openPopup().addTo(Layers.Scaws);
+L.marker([68.986, -36.914], {icon: Icons.ScawsIcon}).bindPopup('Спавн диких').openPopup().addTo(Layers.Scaws);
+L.marker([21.811, 33.047], {icon: Icons.ScawsIcon}).bindPopup('Спавн диких').openPopup().addTo(Layers.Scaws);
+L.marker([34.479, -127.266], {icon: Icons.ScawsIcon}).bindPopup('Спавн диких').openPopup().addTo(Layers.Scaws);
+
+/** ЧВК тестовые Icons */
+L.marker([3.020, -120.586], {icon: Icons.ChvkIcon}).bindPopup('Спавн ЧВК').openPopup().addTo(Layers.Chvk);
+L.marker([-2.954, -10.547], {icon: Icons.ChvkIcon}).bindPopup('Спавн ЧВК').openPopup().addTo(Layers.Chvk);
+L.marker([53.455, -27.070], {icon: Icons.ChvkIcon}).bindPopup('Спавн ЧВК').openPopup().addTo(Layers.Chvk);
+
+
+
+
+
+
+/** OLD CODE PART **/
 
 /** Подключаем хэш в url для учета текущего зума и центра координат пользователя **/
-var hash = new L.Hash(map);
+let hash = new L.Hash(Map);
 
 /** Устанавливаем зум карты на 2 также указываем что минимальный зум 0 а максимальный 2 **/
-map.setMaxZoom(4);
-map.setMinZoom(1);
+Map.setMaxZoom(4);
+Map.setMinZoom(1);
 
 
-/** Получаем текщие координаты по местонахождению мышки **/
+/** Получаем текщие координаты по местонахождению мышки - функция **/
 function onMouseMove(e) {
     $('#mapCoords').text((e.latlng.lat).toFixed(3) + ", " + (e.latlng.lng).toFixed(3));
 }
-map.on('mousemove', onMouseMove);
+
+/** Добавление отслеживания движения мышки по карте **/
+Map.on('mousemove', onMouseMove);
 
 
+/** События по Document Ready */
 $(document).ready(function() {
 
     /** Делаем бэкграунд черным **/
     $('body').css({'background':'black'});
-
-    /*** Объявляем проверочные токены для Ajax ***/
-    var param = $('meta[name=csrf-param]').attr("content");
-    var token = $('meta[name=csrf-token]').attr("content");
 
     /** Отключаем на минимальном зуме кнопку минуса **/
     $('a.leaflet-control-zoom-out').addClass('leaflet-disabled');
 
     /** Возвращаем пользователя к центру карты, если он кликнул на кнопку **/
     $('body').on('click','.mapcenter', function(){
-        map.panTo(new L.LatLng(53, -8));
+        Map.panTo(new L.LatLng(53, -8));
     });
 });
