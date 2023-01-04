5864b897c9ee2bfe582fa844dc9d4287990555ac
PC_Principal <basil2305@ya.ru>
2023-01-04 21:26:34 +0300

    04-01-2023 - Рефакторинг JS файла, по интерактивным картам

--- web/js/streets-of-tarkov-location.js
+++ web/js/streets-of-tarkov-location.js
@@ -1,8 +1,20 @@
 /**
  * JS Обработчик карты - улицы Таркова
+ * Добавляя новые маркеры и группы важно делать следующее:
+ *
+ * - Сетап иконки (Константа Icons)
+ * - Сетап слоя (Константа Layers)
+ * - Сетап чекбокса слоев (Константа LayerControls)
+ *
+ * todo: Сократить громоздкое объявление каждого типа новых UI Элементов (Чекбоксы)
  *
  * @link https://mourner.github.io/Leaflet/reference.html - Documentation LeafletJS
  */
+
+/** Константа с селекторами, для работы с DOM */
+const Selectors = {
+    body: 'body'
+};
 
 /** Константа с опциями карты **/
 const Map = L.map('map', {
@@ -33,6 +45,18 @@ const Icons = {
     ChvkIcon: L.icon({
         iconUrl: '/img/mapicons/chvk.png',
         iconSize: [30, 30]
+    }),
+
+    /** Иконки дверей - отпираемых ключами */
+    KeysIcon: L.icon({
+        iconUrl: '/img/mapicons/keys.png',
+        iconSize: [30, 30]
+    }),
+
+    /** Иконки выдвижных шкафов */
+    ChkafIcon: L.icon({
+        iconUrl: '/img/mapicons/chkaf.png',
+        iconSize: [30, 30]
     })
 };
 
@@ -43,7 +67,22 @@ const Layers = {
     Scaws: L.layerGroup(),
 
     /** Слой маркеров спавнов ЧВК */
-    Chvk: L.layerGroup()
+    Chvk: L.layerGroup(),
+
+    /** Слой дверей, открываемых ключами */
+    Keys: L.layerGroup(),
+
+    /** Слой выдвижных шкафов */
+    Chkaf: L.layerGroup()
+};
+
+/** Ассоциативный набор объектов названий классов чекбоксов и их соответствий в виде Leaflet слоев с маркерами
+ * (Необходимо для работы маркеров и лаконичности их вызова) **/
+const LayerControls = {
+    ScawsControl : Layers.Scaws,
+    ChvkControl : Layers.Chvk,
+    KeysControl : Layers.Keys,
+    ChkafControl : Layers.Chkaf
 };
 
 /** Добавляем базовый слой интерактивной карты (С опцией - без повторения карты) **/
@@ -64,7 +103,7 @@ let ScawsControl = L.Control.extend({
         let div = L.DomUtil.create('div', 'scaws-c');
 
         /** Создание необходимых HTML блоков */
-        div.innerHTML = '<div class="form-control"><input id="ids-control" class="scaws-control" type="checkbox"/>Спавны Диких</div>';
+        div.innerHTML = '<div class="form-control"><input id="ids-control" class="ScawsControl" type="checkbox"/>Спавны Диких</div>';
         return div;
     }
 });
@@ -84,7 +123,47 @@ let ChvkControl = L.Control.extend({
         let div = L.DomUtil.create('div', 'chvk-c');
 
         /** Создание необходимых HTML блоков */
-        div.innerHTML = '<div class="form-control"><input id="ids-control" class="chvk-control" type="checkbox"/>Спавн ЧВК</div>';
+        div.innerHTML = '<div class="form-control"><input id="ids-control" class="ChvkControl" type="checkbox"/>Спавн ЧВК</div>';
+        return div;
+    }
+});
+
+/** Добавляем чекбокс слоя с дверями, октрываемыми ключами - todo: Ниже идут похожие методы, надо сократить */
+let KeysControl = L.Control.extend({
+
+    /** Опции чекбокса (Положение на карте) */
+    options: {
+        position: 'topright'
+    },
+
+    /** Обработчик события при добавлении на карту */
+    onAdd: function () {
+
+        /** Создание необходимых HTML блоков */
+        let div = L.DomUtil.create('div', 'keys-c');
+
+        /** Создание необходимых HTML блоков */
+        div.innerHTML = '<div class="form-control"><input id="ids-control" class="KeysControl" type="checkbox"/>Двери, отпираемые ключами</div>';
+        return div;
+    }
+});
+
+/** Добавляем чекбокс слоя с выдвижными шкафами */
+let ChkafControl = L.Control.extend({
+
+    /** Опции чекбокса (Положение на карте) */
+    options: {
+        position: 'topright'
+    },
+
+    /** Обработчик события при добавлении на карту */
+    onAdd: function () {
+
+        /** Создание необходимых HTML блоков */
+        let div = L.DomUtil.create('div', 'chkaf-c');
+
+        /** Создание необходимых HTML блоков */
+        div.innerHTML = '<div class="form-control"><input id="ids-control" class="ChkafControl" type="checkbox"/>Выдвижные ящики</div>';
         return div;
     }
 });
@@ -92,48 +171,62 @@ let ChvkControl = L.Control.extend({
 /** Добавляем на карту интерактивные элементы */
 Map.addControl(new ScawsControl());
 Map.addControl(new ChvkControl());
+Map.addControl(new KeysControl());
+Map.addControl(new ChkafControl());
 
-/** Функция обработчика чекбокса со спавнами диких (Параметр сюда должен прилететь this */
+/** Функция обработчика чекбокса слоев карты (Параметр сюда должен прилететь this)
+ *
+ * @param Data - Объект this, с которым произошло событие
+ */
 function handleControl(Data) {
 
-    /** В свитче отлавливаем класс чекбокса и в зависимости от состояния слоев, показываем их или нет */
-    switch (Data.className) {
-        case 'scaws-control':
-            if (Data.checked === true) {
-                Map.addLayer(Layers.Scaws)
-            } else {
-                Map.removeLayer(Layers.Scaws)
-            }
-            break;
-        case 'chvk-control': {
-            if (Data.checked === true) {
-                Map.addLayer(Layers.Chvk)
-            } else {
-                Map.removeLayer(Layers.Chvk)
-            }
-            break;
-        }
+    /** Переменная с классом чекбокса, который кликнули */
+    let _className = $(Data).attr('class');
+
+    /** Если чекбокс выбран - делаем активным слой чекбокса, если нет, то отключаем его */
+    if (Data.checked === true) {
+        Map.addLayer(LayerControls[_className]);
+    } else {
+        Map.removeLayer(LayerControls[_className]);
     }
 }
 
 /** Добавляем слушатель событий изменения чекбоксов Leaflet */
-$('body').on('click', '#ids-control', function() {
+$(Selectors.body).on('click', '#ids-control', function() {
 
     /** Вызываем обработчик события и передаем в него this - для дальнейшей работы */
     handleControl(this);
 });
 
-/** Добавляем в слои карты Маркеры (Потом это можно будет сделать нормально - когда данные в бекенд положим (Switch например */
+/** Добавляем в слои карты Маркеры (Данные в хардкоде, в перспективе перенос на Бэкэнд) */
 L.marker([43.299, -18.457], {icon: Icons.ScawsIcon}).bindPopup('Спавн диких').openPopup().addTo(Layers.Scaws);
 L.marker([28.179, -70.664], {icon: Icons.ScawsIcon}).bindPopup('Спавн диких').openPopup().addTo(Layers.Scaws);
 L.marker([68.986, -36.914], {icon: Icons.ScawsIcon}).bindPopup('Спавн диких').openPopup().addTo(Layers.Scaws);
 L.marker([21.811, 33.047], {icon: Icons.ScawsIcon}).bindPopup('Спавн диких').openPopup().addTo(Layers.Scaws);
 L.marker([34.479, -127.266], {icon: Icons.ScawsIcon}).bindPopup('Спавн диких').openPopup().addTo(Layers.Scaws);
 
-/** ЧВК тестовые Icons */
-L.marker([3.020, -120.586], {icon: Icons.ChvkIcon}).bindPopup('Спавн ЧВК').openPopup().addTo(Layers.Chvk);
-L.marker([-2.954, -10.547], {icon: Icons.ChvkIcon}).bindPopup('Спавн ЧВК').openPopup().addTo(Layers.Chvk);
-L.marker([53.455, -27.070], {icon: Icons.ChvkIcon}).bindPopup('Спавн ЧВК').openPopup().addTo(Layers.Chvk);
+/** Добавляем ЧВК маркеры в группы (Данные в хардкоде, в перспективе перенос на Бэкэнд) */
+L.marker([69.936, 50.273], {icon: Icons.ChvkIcon}).bindPopup('Спавн ЧВК').openPopup().addTo(Layers.Chvk);
+L.marker([78.581, -62.402], {icon: Icons.ChvkIcon}).bindPopup('Спавн ЧВК').openPopup().addTo(Layers.Chvk);
+L.marker([72.586, -104.590], {icon: Icons.ChvkIcon}).bindPopup('Спавн ЧВК в кинотеатре Родина').openPopup().addTo(Layers.Chvk);
+L.marker([67.529, -141.592], {icon: Icons.ChvkIcon}).bindPopup('Спавн ЧВК').openPopup().addTo(Layers.Chvk);
+L.marker([61.211, -121.553], {icon: Icons.ChvkIcon}).bindPopup('Спавн ЧВК').openPopup().addTo(Layers.Chvk);
+L.marker([32.739, 1.758], {icon: Icons.ChvkIcon}).bindPopup('Спавн ЧВК в отеле').openPopup().addTo(Layers.Chvk);
+L.marker([42.241, 12.480], {icon: Icons.ChvkIcon}).bindPopup('Спавн ЧВК').openPopup().addTo(Layers.Chvk);
+L.marker([17.073, -81.387], {icon: Icons.ChvkIcon}).bindPopup('Спавн ЧВК').openPopup().addTo(Layers.Chvk);
+
+/** Добавляем маркеры дверей, открываемых ключами (Данные в хардкоде, в перспективе перенос на Бэкэнд) */
+L.marker([72.675, 37.266], {icon: Icons.KeysIcon}).bindPopup('Нужен ключ от Комнаты охраны Concordia').openPopup().addTo(Layers.Keys);
+L.marker([71.005, 50.361], {icon: Icons.KeysIcon}).bindPopup('Нужен ключ от комнаты квартиры Concordia 34 (3 этаж)').openPopup().addTo(Layers.Keys);
+L.marker([71.374, 50.273], {icon: Icons.KeysIcon}).bindPopup('Нужен ключ от квартиры 64 в Concordia').openPopup().addTo(Layers.Keys);
+L.marker([63.415, 24.346], {icon: Icons.KeysIcon}).bindPopup('Нужен ключ от времянки на стройке').openPopup().addTo(Layers.Keys);
+L.marker([49.014, -12.920], {icon: Icons.KeysIcon}).bindPopup('Нужен ключ от железной решетки (3 этаж)').openPopup().addTo(Layers.Keys);
+L.marker([49.758, -108.369], {icon: Icons.KeysIcon}).bindPopup('Нужен Ключ от малого кабинета в финучреждении (2 этаж)').openPopup().addTo(Layers.Keys);
+
+/** Добавляем маркеры выдвижных шкафов */
+L.marker([42.462, -43.506], {icon: Icons.ChkafIcon}).bindPopup('Выдвижной ящик, расположенный на улице').openPopup().addTo(Layers.Chkaf);
+
+
 
 
 
@@ -163,13 +256,13 @@ Map.on('mousemove', onMouseMove);
 $(document).ready(function() {
 
     /** Делаем бэкграунд черным **/
-    $('body').css({'background':'black'});
+    $(Selectors.body).css({'background':'black'});
 
     /** Отключаем на минимальном зуме кнопку минуса **/
     $('a.leaflet-control-zoom-out').addClass('leaflet-disabled');
 
     /** Возвращаем пользователя к центру карты, если он кликнул на кнопку **/
-    $('body').on('click','.mapcenter', function(){
+    $(Selectors.body).on('click','.mapcenter', function(){
         Map.panTo(new L.LatLng(53, -8));
     });
 });
