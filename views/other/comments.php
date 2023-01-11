f4276f66e75be49f6282b26b1ec4bd72205be81b
PC_Principal <basil2305@ya.ru>
2023-01-11 10:41:34 +0300

    11-01-2023 - Добавлен виджет комментариев Anycomment

--- views/other/comments.php
+++ views/other/comments.php
@@ -4,23 +4,29 @@
  * User: basil
  * Date: 01.06.2022
  * Time: 23:42
+ *
  * Виджет комментариев
+ * UPD 10-01-2023 - Anycomments
+ *
+ * @link https://anycomment.io/
  */
 ?>
 
 
 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 
-    <script type="text/javascript">
-        VK.init({
-            apiId: 8182969,
-            onlyWidgets: true
-        });
-    </script>
-
-    <div id="vk_comments"></div>
-    <script type="text/javascript">
-        VK.Widgets.Comments('vk_comments');
+    <div id="anycomment-app"></div>
+    <script>
+        AnyComment = window.AnyComment || []; AnyComment.Comments = [];
+        AnyComment.Comments.push({
+            "root": "anycomment-app",
+            "app_id": 4873,
+            "language": "ru"
+        })
+        var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+        s.src = "https://widget.anycomment.io/comment/embed.js";
+        var sa = document.getElementsByTagName("script")[0];
+        sa.parentNode.insertBefore(s, s.nextSibling);
     </script>
 
 </div>
