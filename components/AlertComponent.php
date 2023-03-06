0de2baf814a5dddaded37884c22604839106941c
PC_Principal <basil2305@ya.ru>
2023-03-06 21:12:58 +0300

    06-03-2023 - API для актуализации квестов [Чиним тесты]

--- components/AlertComponent.php
+++ components/AlertComponent.php
@@ -10,7 +10,6 @@ namespace app\components;
 
 use yii\base\Component;
 use app\models\Info;
-use yii\db\ActiveRecord;
 
 /**
  * Класс для вывода информационной строки на страницах сайта
@@ -24,23 +23,12 @@ class AlertComponent extends Component
      * Компонент бегущей строки, если объект информационная строка, включен
      * то отобразится сообщение виджета из таблицы Info
      *
-     * @return ActiveRecord
+     * @return mixed
      */
-    public static function alert(): ActiveRecord
+    public static function alert()
     {
         $info = Info::find()->where(['id' => 2])->one();
-        return $info;
-    }
-
-    /**
-     * Компонент который выводит курс биткоина
-     *
-     * @return ActiveRecord
-     */
-    public static function bitkoin(): ActiveRecord
-    {
-        $bitkoin = Info::find()->where(['id' => 1])->one();
-        return $bitkoin;
+        return $info ?? null;
     }
 
     /**
@@ -49,10 +37,12 @@ class AlertComponent extends Component
      */
     public static function AlertView(): bool
     {
-        if((AlertComponent::alert()->enabled !== 0)) {
+        /** Если Алерт компонент не равен null - возвращаем true */
+        if((AlertComponent::alert() !== null)) {
             return true;
         }
 
+        /** В противном случае false */
         return false;
     }
 }
--- tests/_data/boss.php
+++ tests/_data/boss.php
@@ -8,6 +8,7 @@
  * Фикстура с боссами Резерва, для функционального тестирования детальных страниц боссов
  */
 return [
+    'id' => 10,
     'map' => 'Резерв',
     'bosses' => '[{"name":"Глухарь","spawnChance":0.2,"spawnTrigger":null,"spawnLocations":[{"name":"RailStrorage"},{"name":"PTOR1"},{"name":"PTOR2"},{"name":"Barrack"},{"name":"SubStorage"}],"escorts":[{"amount":[{"count":2}]},{"amount":[{"count":2}]},{"amount":[{"count":2}]}]},{"name":"Рейдер","spawnChance":0.3,"spawnTrigger":null,"spawnLocations":[{"name":"RailStrorage"}],"escorts":[{"amount":[{"count":2},{"count":3}]}]},{"name":"Рейдер","spawnChance":0.35,"spawnTrigger":"Bunker Hermetic Door Power Switch","spawnLocations":[{"name":"RailStrorage"}],"escorts":[{"amount":[{"count":2},{"count":3}]}]},{"name":"Рейдер","spawnChance":0.25,"spawnTrigger":"D-2 Power Switch","spawnLocations":[{"name":"SubCommand"}],"escorts":[{"amount":[{"count":2},{"count":3}]}]},{"name":"Рейдер","spawnChance":0.15,"spawnTrigger":null,"spawnLocations":[{"name":"SubCommand"}],"escorts":[{"amount":[{"count":2},{"count":3}]}]}]',
     'date_create' => date("Y-m-d H:i:s"),
