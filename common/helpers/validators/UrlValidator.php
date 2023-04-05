7ed98575814219409dcd60132bd4c309a3409a57
PC_Principal <basil2305@ya.ru>
2023-04-05 22:59:39 +0300

    05-04-2023 - Рестрикт на регистрацию кланов без заполненной ссылки на клан

---
+++ common/helpers/validators/UrlValidator.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: PC_Principal
+ * Date: 05.04.2023
+ * Time: 22:53
+ */
+
+namespace app\common\helpers\validators;
+
+/**
+ * Валидатор строки - является ли URL адресом
+ *
+ * Class UrlValidator
+ * @package app\common\helpers\validators
+ */
+class UrlValidator extends \yii\validators\UrlValidator
+{
+    const ATTR_VALID_SCHEMES = 'validSchemes';
+}
