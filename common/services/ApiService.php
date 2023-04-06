71d6a435d6fae1fdd031486cbb9facaef54cc537
PC_Principal <basil2305@ya.ru>
2023-04-06 19:13:45 +0300

    06-04-2023 [API] - Хранение предмета в течение всего 1-го дня в базе

--- common/services/ApiService.php
+++ common/services/ApiService.php
@@ -579,6 +579,7 @@ final class ApiService implements ApiInterface
 
     /**
      * Метод проставляющий устаревание для предметов, полученных по API
+     * UPD. 06.04.2023 - Сделали дату устаревения записи на 1 день
      *
      * @return bool
      */
@@ -591,9 +592,9 @@ final class ApiService implements ApiInterface
         foreach ($items as $item) {
 
             /** Дата устаревания записи */
-            $date = date('Y-m-d H:i:s', strtotime($item->date_create . ' +3 month'));
+            $date = date('Y-m-d H:i:s', strtotime($item->date_create . ' +1 day'));
 
-            /** Если дата записи +3 месяца - меньше текущего времени - запись должна быть помечена на удаление */
+            /** Если дата записи +1 день - меньше текущего времени - запись должна быть помечена на удаление */
             if ($date < date("Y-m-d H:i:s",time())) {
 
                 /** Устанавливаем флаг старой записи */
