f9310b1ec7d017808d284a5061ed3553ed98d2d7
PC_Principal <basil2305@ya.ru>
2023-10-29 00:48:25 +0300

    29-10-2023г. - Functional и Unit тесты лута из Api

--- models/ApiLoot.php
+++ models/ApiLoot.php
@@ -20,7 +20,6 @@ use yii\helpers\Json;
  * @property string $url URL адрес
  * @property string $date_create Дата создания записи о предмете
  * @property int $active Флаг активности записи
- * @property int $old Флаг возраста записи, если стоит 1, пора удалять
  */
 class ApiLoot extends ActiveRecord
 {
@@ -31,7 +30,6 @@ class ApiLoot extends ActiveRecord
     const ATTR_URL  = 'url';
     const ATTR_DATE_CREATE = 'date_create';
     const ATTR_ACTIVE = 'active';
-    const ATTR_OLD = 'old';
 
     /** Константы bool значений */
     const TRUE = 1;
@@ -66,9 +64,7 @@ class ApiLoot extends ActiveRecord
 
             [static::ATTR_DATE_CREATE, SafeValidator::class],
 
-            [static::ATTR_ACTIVE, IntegerValidator::class],
-
-            [static::ATTR_OLD, IntegerValidator::class]
+            [static::ATTR_ACTIVE, IntegerValidator::class]
         ];
     }
 
