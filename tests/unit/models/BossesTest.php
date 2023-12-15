bae12e3d5034776b9e764ea83d1240b4d4fb6c12
PC_Principal <basil2305@ya.ru>
2023-12-15 20:33:13 +0300

    15-12-2023г. - Unit тесты страниц боссов

--- tests/unit/models/BossesTest.php
+++ tests/unit/models/BossesTest.php
@@ -6,10 +6,11 @@
  * Time: 23:51
  */
 
-namespace models;
+namespace app\tests;
 
 use app\models\Bosses;
 use app\tests\fixtures\BossesFixture;
+use app\common\helpers\validators\StringValidator;
 
 /**
  * Unit тесты для API страниц боссов
@@ -27,6 +28,13 @@ class BossesTest extends \Codeception\Test\Unit
     /** Метод выполняется перед каждым тестом */
     protected function _before()
     {
+        /** Грузим фикстуры перед каждым тестом */
+        $this->tester->haveFixtures([
+            'bosses' => [
+                'class' => BossesFixture::class,
+                'dataFile' => codecept_data_dir() . 'bosses.php'
+            ]
+        ]);
     }
 
     /** Метод выполняется после каждого теста */
@@ -34,76 +42,164 @@ class BossesTest extends \Codeception\Test\Unit
     {}
 
     /**
-     * Фикстуры для таблицы bosses
-     * @return array
+     * Метод вызывающий валидации атрибутов различных типов
      */
-    public function _fixtures()
+    protected function _validateAttributes($model)
     {
-        return [
-            'bosses' => [
-                'class' => BossesFixture::class,
-                'dataFile' => codecept_data_dir() . 'bosses.php'
-            ]
+        /** Валидация обязательных атрибутов */
+        $this->_validateRequiredAttributes($model);
+
+        /** Валидация строковых атрибутов */
+        $this->_validateStringAttributes($model);
+
+        /** Валидация числовых атрибутов */
+        $this->_validateNumberAttributes($model);
+    }
+
+    /** Метод для валидации обязательных атрибутов */
+    protected function _validateRequiredAttributes($model)
+    {
+        /** Список атрибутов на валидацию */
+        $list = [];
+
+        /** Проходим в цикле список атрибутов */
+        foreach ($list as $item) {
+
+            /** Пробуем оставить их как null */
+            $this->_validateAttribute($model, $item, null);
+        }
+    }
+
+    /** Метод для валидации числовых атрибутов */
+    protected function _validateNumberAttributes($model)
+    {
+        /** Список атрибутов на валидацию */
+        $list = [Bosses::ATTR_ID, Bosses::ATTR_ACTIVE, Bosses::ATTR_OLD];
+
+        /** Проходим в цикле список атрибутов */
+        foreach ($list as $item) {
+
+            /** Пробуем засетапить в числовой атрибут - строку */
+            $this->_validateAttribute($model, $item, 'a');
+        }
+    }
+
+    /** Метод для валидации строковых атрибутов */
+    protected function _validateStringAttributes($model)
+    {
+        /** Список атрибутов на валидацию - длина 100 символов */
+        $list_hundred = [Bosses::ATTR_MAP];
+
+        /** Список атрибутов на валидацию - длина 255 символов */
+        $list_main = [Bosses::ATTR_URL];
+
+        /** Переменная с пустой строкой */
+        $too_long_string = '';
+
+        /** В цикле увеличиваем длину строки, пока не станет 101 символов */
+        for ($i = 0; $i < StringValidator::VARCHAR_LENGTH_HUNDRED + 1; $i++) {
+            $too_long_string .= 'a';
+        }
+
+        /** В цикле увеличиваем длину строки, пока не станет 101 символов */
+        foreach ($list_hundred as $item) {
+
+            /** Валидируем каждый из них */
+            $this->_validateAttribute($model, $item, $too_long_string);
+        }
+
+        /** В цикле увеличиваем длину строки, пока не станет 256 символов */
+        for ($i = 0; $i < StringValidator::VARCHAR_LENGTH + 1; $i++) {
+            $too_long_string .= 'a';
+        }
+
+        /** Проходим в цикле список атрибутов - длина строки 256 символов */
+        foreach ($list_main as $item) {
+
+            /** Валидируем каждый из них */
+            $this->_validateAttribute($model, $item, $too_long_string);
+        }
+    }
+
+    /** Метод валидации атрибута, что сюда передается */
+    protected function _validateAttribute($model, $attribute, $value)
+    {
+        /** Сетапим значение атрибута AR модели */
+        $model->setAttribute($attribute, $value);
+
+        /** Ожидаем что атрибут не пройдет валидацию */
+        $this->assertFalse($model->validate($attribute), $attribute . ': ' . $value);
+    }
+
+    /** Тестируем создание нового маркера */
+    public function testCreation()
+    {
+        /** Создаем новый объект AR */
+        $item = new Bosses();
+
+        /** Валидируем все атрибуты AR объекта*/
+        $this->_validateAttributes($item);
+
+        /** Значения на сохранение нового объекта */
+        $values = [
+            Bosses::ATTR_ID     => 10,
+            Bosses::ATTR_MAP    => 'Новая локация',
+            Bosses::ATTR_BOSSES => '[{"name":"Death Knight","spawnChance":0.16,"spawnTrigger":null,"spawnLocations":[{"name":"ScavBase"}],"escorts":[{"amount":[{"count":2}]},{"amount":[{"count":1}]},{"amount":[{"count":1}]}]},{"name":"Решала","spawnChance":0.1,"spawnTrigger":null,"spawnLocations":[{"name":"Dormitory"},{"name":"GasStation"}],"escorts":[{"amount":[{"count":4}]}]},{"name":"Сектант Жрец","spawnChance":0.02,"spawnTrigger":null,"spawnLocations":[{"name":"ScavBase"}],"escorts":[{"amount":[{"count":4}]}]}]',
+            Bosses::ATTR_ACTIVE => 1,
+            Bosses::ATTR_OLD    => 0,
+            Bosses::ATTR_URL    => 'new_location'
+
         ];
+
+        /** Сетапим атрибуты AR объекту */
+        $item->setAttributes($values);
+
+        /** Валидируем атрибуты */
+        $item->validate();
+
+        /** Ожидаем что запись сохранилась */
+        $this->assertTrue($item->save(), 'Ожидалось true - объект не сохранился.');
+
+        /** Выбираем все записи */
+        $list = Bosses::find()->all();
+
+        /** Ожидаем что всего будет 4 записи */
+        $this->assertTrue(count($list) == 10);
     }
 
-    /** Тестируем создание */
-    public function testCreate()
+    /** Тестируем выборку маркера на обновление */
+    public function testEdit()
     {
-        $boss = new Bosses();
+        /** Выбираем одну из записей, представленных в фикстурах */
+        $item = Bosses::findOne([Bosses::ATTR_ID => 3]);
 
-        $boss->map = 'Таможня';
-        $boss->bosses = '[{"name":"Death Knight","spawnChance":0.16,"spawnTrigger":null,"spawnLocations":[{"name":"ScavBase"}],"escorts":[{"amount":[{"count":2}]},{"amount":[{"count":1}]},{"amount":[{"count":1}]}]},{"name":"Решала","spawnChance":0.1,"spawnTrigger":null,"spawnLocations":[{"name":"Dormitory"},{"name":"GasStation"}],"escorts":[{"amount":[{"count":4}]}]},{"name":"Сектант Жрец","spawnChance":0.02,"spawnTrigger":null,"spawnLocations":[{"name":"ScavBase"}],"escorts":[{"amount":[{"count":4}]}]}]';
-        $boss->active = 1;
-        $boss->old = 0;
-        $boss->url = 'tamojnya';
-
-        $this->assertTrue($boss->save(), 'Ожидалось true, вернулось false - объект не сохранился.');
+        /** Проводит валидацию атрибутов данных, полученных из фикстуры */
+        $this->_validateAttributes($item);
     }
 
-    /** Тестируем обновление */
-    public function testUpdate()
+    /** Тестируем получение всех записей (select) */
+    public function testList()
     {
-        $boss = Bosses::find()->where(['is not', 'id', null])->one();
+        /** Выбираем все записи */
+        $list = Bosses::find()->all();
 
-        $boss->map = 'Таможня33';
-        $boss->bosses = '[{"name":"Death Knight","spawnChance":0.16,"spawnTrigger":null,"spawnLocations":[{"name":"ScavBase"}],"escorts":[{"amount":[{"count":2}]},{"amount":[{"count":1}]},{"amount":[{"count":1}]}]},{"name":"Решала","spawnChance":0.1,"spawnTrigger":null,"spawnLocations":[{"name":"Dormitory"},{"name":"GasStation"}],"escorts":[{"amount":[{"count":4}]}]},{"name":"Сектант Жрец","spawnChance":0.02,"spawnTrigger":null,"spawnLocations":[{"name":"ScavBase"}],"escorts":[{"amount":[{"count":4}]}]}]';
-        $boss->active = 0;
-        $boss->old = 1;
-        $boss->url = 'tamojnya';
-
-        $this->assertIsInt($boss->update(), 'Ожидался int, вернулся false - объект не обновился.');
-    }
-
-    /** Тестируем получение объекта (select) */
-    public function testSelect()
-    {
-        $boss = Bosses::find()->one();
-
-        $this->assertNotNull($boss, 'Ожидался объект, вернулся null - объект не селектнулся.');
-    }
-
-    /** Тестируем получение всех объектов (select all) */
-    public function testSelectAll()
-    {
-        $boss = Bosses::find()->all();
-
-        $this->assertTrue(!empty($boss), 'Ожидалось что вернутся объекты, этого не случилось - что-то пошло не так');
+        /** Ожидаем получить из фикстур - 3 записи */
+        $this->assertTrue(count($list) == 9);
     }
 
     /** Тестируем удаление объекта */
     public function testDelete()
     {
-        $boss = Bosses::find()->one()->delete();
+        /** Выбираем одну из записей, представленных в фикстурах */
+        $item = Bosses::findOne([Bosses::ATTR_ID => 3]);
 
-        $this->assertIsInt($boss, 'Удаление объекта не случилось, а должно было.');
-    }
+        /** Удаляем запись */
+        $item->delete();
 
-    /** Тестируем удаление всех объектов */
-    public function testDeleteAll()
-    {
-        $boss = Bosses::deleteAll();
+        /** Получаем список всех записей */
+        $list = Bosses::find()->all();
 
-        $this->assertIsInt($boss, 'Удаление объекта не случилось, а должно было.');
+        /** Ожидаем получить из фикстур - 2 записи */
+        $this->assertTrue(count($list) == 8);
     }
 }
