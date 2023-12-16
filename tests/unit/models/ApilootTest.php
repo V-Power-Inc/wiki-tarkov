81cbc2523bbede2d3060497989007bb275baf3a6
PC_Principal <basil2305@ya.ru>
2023-12-16 13:07:44 +0300

    16-12-2023г. - Расширили Unit тест API лута

--- tests/unit/models/ApilootTest.php
+++ tests/unit/models/ApilootTest.php
@@ -130,7 +130,7 @@ class ApilootTest extends \Codeception\Test\Unit
 
         /** Значения на сохранение нового объекта */
         $values = [
-            ApiLoot::ATTR_ID     => 4,
+            ApiLoot::ATTR_ID     => 22,
             ApiLoot::ATTR_NAME   => 'M4A1-Автомат',
             ApiLoot::ATTR_JSON   => '{"id":"5ae30e795acfc408fb139a0b","name":"Мушка-газблок для M4A1","normalizedName":"m4a1-front-sight-with-gas-block","width":1,"height":1,"weight":0.15,"description":"Штатная мушка для M4A1, производство Colt.","category":{"name":"Газовый блок"},"iconLink":"https://assets.tarkov.dev/5ae30e795acfc408fb139a0b-icon.webp","inspectImageLink":"https://assets.tarkov.dev/5ae30e795acfc408fb139a0b-image.webp","sellFor":[{"vendor":{"name":"Прапор"},"price":1285,"currency":"RUB","currencyItem":{"name":"Рубли"},"priceRUB":1285},{"vendor":{"name":"Скупщик"},"price":1028,"currency":"RUB","currencyItem":{"name":"Рубли"},"priceRUB":1028},{"vendor":{"name":"Лыжник"},"price":1259,"currency":"RUB","currencyItem":{"name":"Рубли"},"priceRUB":1259},{"vendor":{"name":"Миротворец"},"price":9,"currency":"USD","currencyItem":{"name":"Доллары"},"priceRUB":1156},{"vendor":{"name":"Механик"},"price":1439,"currency":"RUB","currencyItem":{"name":"Рубли"},"priceRUB":1439},{"vendor":{"name":"Барахолка"},"price":20000,"currency":"RUB","currencyItem":{"name":"Рубли"},"priceRUB":20000}],"buyFor":[{"vendor":{"name":"Миротворец"},"price":21,"currency":"USD","currencyItem":{"name":"Доллары"},"priceRUB":3125},{"vendor":{"name":"Барахолка"},"price":26255,"currency":"RUB","currencyItem":{"name":"Рубли"},"priceRUB":26255}],"bartersFor":[],"receivedFromTasks":[{"name":"Друг с Запада - Часть 1","trader":{"name":"Лыжник"}}]}',
             ApiLoot::ATTR_URL    => 'm4a1',
@@ -149,21 +149,8 @@ class ApilootTest extends \Codeception\Test\Unit
         /** Выбираем все записи */
         $list = ApiLoot::find()->all();
 
-        /** Ожидаем что всего будет 4 записи */
-        $this->assertTrue(count($list) == 4);
-    }
-
-    /** Тестируем создание */
-    public function testCreate()
-    {
-        $loot = new ApiLoot();
-
-        $loot->name = 'M4A1-Автомат';
-        $loot->json = '{"id":"5ae30e795acfc408fb139a0b","name":"Мушка-газблок для M4A1","normalizedName":"m4a1-front-sight-with-gas-block","width":1,"height":1,"weight":0.15,"description":"Штатная мушка для M4A1, производство Colt.","category":{"name":"Газовый блок"},"iconLink":"https://assets.tarkov.dev/5ae30e795acfc408fb139a0b-icon.webp","inspectImageLink":"https://assets.tarkov.dev/5ae30e795acfc408fb139a0b-image.webp","sellFor":[{"vendor":{"name":"Прапор"},"price":1285,"currency":"RUB","currencyItem":{"name":"Рубли"},"priceRUB":1285},{"vendor":{"name":"Скупщик"},"price":1028,"currency":"RUB","currencyItem":{"name":"Рубли"},"priceRUB":1028},{"vendor":{"name":"Лыжник"},"price":1259,"currency":"RUB","currencyItem":{"name":"Рубли"},"priceRUB":1259},{"vendor":{"name":"Миротворец"},"price":9,"currency":"USD","currencyItem":{"name":"Доллары"},"priceRUB":1156},{"vendor":{"name":"Механик"},"price":1439,"currency":"RUB","currencyItem":{"name":"Рубли"},"priceRUB":1439},{"vendor":{"name":"Барахолка"},"price":20000,"currency":"RUB","currencyItem":{"name":"Рубли"},"priceRUB":20000}],"buyFor":[{"vendor":{"name":"Миротворец"},"price":21,"currency":"USD","currencyItem":{"name":"Доллары"},"priceRUB":3125},{"vendor":{"name":"Барахолка"},"price":26255,"currency":"RUB","currencyItem":{"name":"Рубли"},"priceRUB":26255}],"bartersFor":[],"receivedFromTasks":[{"name":"Друг с Запада - Часть 1","trader":{"name":"Лыжник"}}]}';
-        $loot->url  = 'm4a1';
-        $loot->active = 1;
-
-        $this->assertTrue($loot->save(), 'Ожидалось true, вернулось false - объект не сохранился.');
+        /** Ожидаем что всего будет 22 записи */
+        $this->assertTrue(count($list) == 22);
     }
 
     /** Тестируем выборку записи на обновление */
@@ -182,9 +169,49 @@ class ApilootTest extends \Codeception\Test\Unit
         /** Выбираем все записи */
         $list = ApiLoot::find()->all();
 
-        /** Ожидаем получить из фикстур - 3 записи */
-        $this->assertTrue(count($list) == 3);
+        /** Ожидаем получить из фикстур - 21 записи */
+        $this->assertTrue(count($list) == 21);
     }
+
+    /** Метод для получения только активных записей */
+    public function testSelectActiveRows()
+    {
+        /** Выбираем все записи */
+        $list = ApiLoot::find()
+            ->where([ApiLoot::ATTR_ACTIVE => 1])
+            ->all();
+
+        /** Ожидаем получить из фикстур - 21 записи */
+        $this->assertTrue(count($list) == 20);
+    }
+
+    /** Метод для получения только активных записей с определенными филдами и сортировкой (как в контроллере) */
+    public function testSelectActiveRowsWithSortAndOrder()
+    {
+        /** Выбираем все записи используя селект и сортировку (как в контроллере) */
+        $list = ApiLoot::find()
+            ->select([ApiLoot::ATTR_JSON, ApiLoot::ATTR_URL])
+            ->where([ApiLoot::ATTR_ACTIVE => ApiLoot::TRUE])
+            ->orderBy([ApiLoot::ATTR_DATE_CREATE => SORT_DESC])
+            ->all();
+
+        /** Ожидаем получить из фикстур - 20 записи */
+        $this->assertTrue(count($list) == 20);
+    }
+
+    /** Метод для получения только одной активной записи по урлу */
+    public function testSelectActiveByUrlSingleRow()
+    {
+        /** Выбираем все записи */
+        $item = ApiLoot::find()
+            ->where([ApiLoot::ATTR_URL => 'm4a1-front-sight-with-gas-block-one'])
+            ->andWhere([ApiLoot::ATTR_ACTIVE => 1])
+            ->one();
+
+        /** Ожидаем получить из фикстур - 1 запись */
+        $this->assertTrue(!empty($item));
+    }
+
 
     /** Тестируем удаление объекта */
     public function testDelete()
@@ -198,7 +225,7 @@ class ApilootTest extends \Codeception\Test\Unit
         /** Получаем список всех записей */
         $list = ApiLoot::find()->all();
 
-        /** Ожидаем получить из фикстур - 2 записи */
-        $this->assertTrue(count($list) == 2);
+        /** Ожидаем получить из фикстур - 20 записи */
+        $this->assertTrue(count($list) == 20);
     }
 }
