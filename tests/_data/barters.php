72fb2848f3a68024c1d6106c165a830b43ea7361
PC_Principal <basil2305@ya.ru>
2023-12-18 00:18:16 +0300

    18-12-2023г. - Добавлен Unit тест бартеров

---
+++ tests/_data/barters.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: PC_Principal
+ * Date: 17.12.2023
+ * Time: 23:50
+ *
+ * Фикстуры бартеров торговцев
+ */
+return [
+    [
+        'title' => 'Прапор - 1 уровень',
+        'content' => '<ul><li><span style="font-size:16px">Разнообразие <a href="https://wiki-tarkov.ru/loot/ammunition" target="_blank">патронов</a> для <a href="https://wiki-tarkov.ru/loot/weapons/pistols" target="_blank">пистолетов</a> и <a href="https://wiki-tarkov.ru/loot/weapons/submachine-guns" target="_blank">пистолетов-пулеметов</a>.</span></li><li><span style="font-size:16px"><a href="https://wiki-tarkov.ru/loot/marker.html" target="_blank">Маркер MS2000</a> -&nbsp;необходим для заданий, где нужно заложить маячок.</span></li><li><span style="font-size:16px">В продаже появляются <a href="https://wiki-tarkov.ru/loot/ammunition" target="_blank">патроны</a> <a href="https://wiki-tarkov.ru/loot/ammunition/5-45-39" target="_blank">5.45x39 мм</a>&nbsp;<a href="https://wiki-tarkov.ru/loot/5-45-39-ps.html" style="box-sizing: border-box; background-color: transparent; color: rgb(51, 122, 183); text-decoration-line: none;">ПС</a>&nbsp;при завершенном квесте &quot;<a href="https://wiki-tarkov.ru/quests-of-traders/prapor-quests#4" style="box-sizing: border-box; background-color: transparent; color: rgb(51, 122, 183); text-decoration-line: none;">Посылка из прошлого</a>&quot;.</span></li><li><span style="font-size:16px"><a href="https://wiki-tarkov.ru/loot/tactec-rig.html" target="_blank">Бронеразгрузка TacTec</a> в обмен на предметы.</span></li><li><span style="font-size:16px"><a href="https://wiki-tarkov.ru/loot/mosinka.html" target="_blank">Винтовка системы Мосина</a> за рубли и в обмен на предметы.</span></li></ul>',
+        'enabled' => 1,
+        'site_title' => 'УЛ 1',
+        'trader_group' => 'Прапор'
+    ],
+    [
+        'title' => 'Прапор - 2 уровень',
+        'content' => '<ul><li><span style="font-size:16px">Разнообразие <a href="https://wiki-tarkov.ru/loot/ammunition" target="_blank">патронов</a> для <a href="https://wiki-tarkov.ru/loot/weapons/pistols" target="_blank">пистолетов</a> и <a href="https://wiki-tarkov.ru/loot/weapons/submachine-guns" target="_blank">пистолетов-пулеметов</a>.</span></li><li><span style="font-size:16px"><a href="https://wiki-tarkov.ru/loot/marker.html" target="_blank">Маркер MS2000</a> -&nbsp;необходим для заданий, где нужно заложить маячок.</span></li><li><span style="font-size:16px">В продаже появляются <a href="https://wiki-tarkov.ru/loot/ammunition" target="_blank">патроны</a> <a href="https://wiki-tarkov.ru/loot/ammunition/5-45-39" target="_blank">5.45x39 мм</a>&nbsp;<a href="https://wiki-tarkov.ru/loot/5-45-39-ps.html" style="box-sizing: border-box; background-color: transparent; color: rgb(51, 122, 183); text-decoration-line: none;">ПС</a>&nbsp;при завершенном квесте &quot;<a href="https://wiki-tarkov.ru/quests-of-traders/prapor-quests#4" style="box-sizing: border-box; background-color: transparent; color: rgb(51, 122, 183); text-decoration-line: none;">Посылка из прошлого</a>&quot;.</span></li><li><span style="font-size:16px"><a href="https://wiki-tarkov.ru/loot/tactec-rig.html" target="_blank">Бронеразгрузка TacTec</a> в обмен на предметы.</span></li><li><span style="font-size:16px"><a href="https://wiki-tarkov.ru/loot/mosinka.html" target="_blank">Винтовка системы Мосина</a> за рубли и в обмен на предметы.</span></li></ul>',
+        'enabled' => 1,
+        'site_title' => 'УЛ 2',
+        'trader_group' => 'Прапор'
+    ],
+    [
+        'title' => 'Механик - 1 уровень',
+        'content' => '<ul><li><span style="font-size:16px">Разнообразие <a href="https://wiki-tarkov.ru/loot/ammunition" target="_blank">патронов</a> для <a href="https://wiki-tarkov.ru/loot/weapons/pistols" target="_blank">пистолетов</a> и <a href="https://wiki-tarkov.ru/loot/weapons/submachine-guns" target="_blank">пистолетов-пулеметов</a>.</span></li><li><span style="font-size:16px"><a href="https://wiki-tarkov.ru/loot/marker.html" target="_blank">Маркер MS2000</a> -&nbsp;необходим для заданий, где нужно заложить маячок.</span></li><li><span style="font-size:16px">В продаже появляются <a href="https://wiki-tarkov.ru/loot/ammunition" target="_blank">патроны</a> <a href="https://wiki-tarkov.ru/loot/ammunition/5-45-39" target="_blank">5.45x39 мм</a>&nbsp;<a href="https://wiki-tarkov.ru/loot/5-45-39-ps.html" style="box-sizing: border-box; background-color: transparent; color: rgb(51, 122, 183); text-decoration-line: none;">ПС</a>&nbsp;при завершенном квесте &quot;<a href="https://wiki-tarkov.ru/quests-of-traders/prapor-quests#4" style="box-sizing: border-box; background-color: transparent; color: rgb(51, 122, 183); text-decoration-line: none;">Посылка из прошлого</a>&quot;.</span></li><li><span style="font-size:16px"><a href="https://wiki-tarkov.ru/loot/tactec-rig.html" target="_blank">Бронеразгрузка TacTec</a> в обмен на предметы.</span></li><li><span style="font-size:16px"><a href="https://wiki-tarkov.ru/loot/mosinka.html" target="_blank">Винтовка системы Мосина</a> за рубли и в обмен на предметы.</span></li></ul>',
+        'enabled' => 0,
+        'site_title' => 'УЛ 1',
+        'trader_group' => 'Механик'
+    ]
+];
