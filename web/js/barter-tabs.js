77b11ce4e92402c65c430af0d35ef4600c029f74
PC_Principal <basil2305@ya.ru>
2023-12-26 17:21:20 +0300

    26-12-2023г. - Небольшой рефакторинг

--- web/js/barter-tabs.js
+++ web/js/barter-tabs.js
@@ -1,30 +1,13 @@
 /**
  * Created by comp on 28.08.2018.
+ *
+ * JS для бартеров торговцев
  */
 
 $(document).ready(function() {
-
-    console.log(window.location.hash);
 
     /** Если в URL не было хэща - кликаем на первый таб **/
     if (window.location.hash === '') {
         $('.first-lvl').click();
     }
-
-    /** События при изменении хэша - доработки под вопросом **/
-    // $(function() {
-    //     $('a[data-toggle="tab"]').on('shown.bs.tab', function () {
-    //         window.location.hash = $(this).attr('href');
-    //     });
-    //
-    //     if (window.location.hash) {
-    //         $('' + window.location.hash + '').trigger('click');
-    //     }
-    //
-    //     $(window).on('hashchange', function() {
-    //         $('' + window.location.hash + '').trigger('click');
-    //     });
-    // });
-
 });
-
