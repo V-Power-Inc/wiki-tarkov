00347aa49360b500f265dec9c8df00c921e8a3ae
PC_Principal <basil2305@ya.ru>
2024-03-13 18:37:16 +0300

    13-03-2024г. - Перезапись обработчика ошибок для модуля админки

--- modules/admin/Admin.php
+++ modules/admin/Admin.php
@@ -2,6 +2,9 @@
 
 namespace app\modules\admin;
 
+use yii\web\ErrorHandler;
+use Yii;
+
 /**
  * admin module definition class
  *
@@ -16,10 +19,32 @@ class Admin extends \yii\base\Module
     public $controllerNamespace = 'app\modules\admin\controllers';
 
     /**
+     * Не существует такой вещи, как обработчик ошибок модуля, поэтому нам придется перезаписать обработчик ошибок приложения.
+     *
      * @inheritdoc
      */
     public function init()
     {
+        /** Возвращаем родительский конструктор */
         parent::init();
+
+        /** На лету переопределяем обработчик ошибок, у админки он свой, со своей страницей ошибок */
+        Yii::configure($this, [
+            'components' => [
+                'errorHandler' => [
+                    'class' => ErrorHandler::class,
+                    'errorAction' => 'admin/default/error'
+                ]
+            ],
+        ]);
+
+        /** Получаем созданный обработчик ошибок */
+        $handler = $this->get('errorHandler');
+
+        /** Перезаписываем обработчик событий тем, что создали только что */
+        Yii::$app->set('errorHandler', $handler);
+
+        /** Регистрируем его */
+        $handler->register();
     }
 }
---
+++ modules/admin/views/default/error.php
@@ -0,0 +1,31 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $name string */
+/* @var $message string */
+/* @var $exception Exception */
+
+use yii\helpers\Html;
+
+/** Сетапим title в соответствии с именем ошибки */
+$this->title = $name;
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="site-error">
+
+                <h1><?= Html::encode($this->title) ?></h1>
+
+                <div class="alert alert-danger">
+                    <?= nl2br(Html::encode($message)) ?>
+                </div>
+
+                <button class="btn btn-primary"><a href="/admin" style="color: white; text-decoration: none;">Вернуться на главную</a></button>
+
+            </div>
+        </div>
+    </div>
+</div>
+
