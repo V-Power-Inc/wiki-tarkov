08022518abab79957159a2e12359ab08ffe2a5eb
PC_Principal <basil2305@ya.ru>
2024-03-21 15:27:09 +0300

    21-03-2024г. - Unit тесты для DbCommandService

---
+++ tests/unit/services/DbCommandServiceTest.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: PC_Principal
+ * Date: 21.03.2024
+ * Time: 15:21
+ */
+
+namespace app\common\services;
+
+use yii\db\Query;
+use yii\db\Command;
+use Codeception\Test\Unit;
+use yii\db\Exception as DbException;
+
+/**
+ * Тест сервиса по DB командам
+ *
+ * Class DbCommandServiceTest
+ * @package app\common\services
+ */
+class DbCommandServiceTest extends Unit
+{
+    /**
+     * Тестируем метод сервиса для генерации SQL комманд через createCommand Yii
+     *
+     * @return void
+     * @throws DbException
+     */
+    public function testCreateCommandQueryAll()
+    {
+        /** Подготавливаем мок объекта Query */
+        $query = $this->getMockBuilder(Query::class)
+            ->setMethods(['createCommand'])
+            ->getMock();
+
+        /** Подготавливаем мок объекта Command */
+        $command = $this->getMockBuilder(Command::class)
+            ->setMethods(['queryAll'])
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        /** Устанавливаем ожидаемое значение для метода queryAll */
+        $expectedResult = [['id' => 1, 'name' => 'John'], ['id' => 2, 'name' => 'Jane']];
+        $command->method('queryAll')->willReturn($expectedResult);
+
+        /** Устанавливаем ожидаемое значение для метода createCommand */
+        $query->expects($this->once())
+            ->method('createCommand')
+            ->willReturn($command);
+
+        /** Вызываем тестируемый метод */
+        $result = DbCommandService::createCommandQueryAll($query);
+
+        /** Проверяем, что возвращенный результат соответствует ожидаемому */
+        $this->assertEquals($expectedResult, $result);
+    }
+}
