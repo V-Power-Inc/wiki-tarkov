db7b902e029e0316fe4c266f37724943bd80ebd6
PC_Principal <basil2305@ya.ru>
2024-04-06 11:24:40 +0300

    06-04-2024г. - Unit тесты для LogService

---
+++ tests/unit/services/LogServiceTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace app\tests;
+
+use app\common\interfaces\ResponseStatusInterface;
+use app\common\services\LogService;
+use app\models\ErrorLog;
+use Codeception\Test\Unit;
+use tests\_support\FixturesCollection;
+use UnitTester;
+
+/**
+ * Тестируем сервис по сохранению лога ошибок в БД
+ *
+ * Class LogServiceTest
+ * @package app\tests
+ */
+class LogServiceTest extends Unit
+{
+    /** @var UnitTester - Объект класса для тестирования */
+    protected UnitTester $tester;
+
+    /**
+     * Действия выполняемые перед каждым тестом
+     *
+     * @return void
+     */
+    public function _before()
+    {
+        /** Грузим фикстуры перед каждым тестом */
+        $this->tester->haveFixtures(FixturesCollection::getErrors());
+    }
+
+    /**
+     * Тестирование метода по сохранению ошибок в БД
+     *
+     * @return void
+     */
+    public function testSaveErrorData()
+    {
+        /** Тестовые данные */
+        $url = '/example-url';
+        $type = 'test';
+        $description = 'This is a test error description';
+        $errorCode = ResponseStatusInterface::SERVER_ERROR_CODE;
+
+        /** Дергаем тестируемый метод */
+        $result = LogService::saveErrorData($url, $type, $description, $errorCode);
+
+        /** Проверяем, что метод вернул true (т.е. данные успешно сохранены) */
+        $this->assertTrue($result);
+
+        /** Проверяем, что данные были сохранены в базу данных */
+        $errorLog = ErrorLog::findOne(['url' => $_ENV['DOMAIN_PROTOCOL'] . $_ENV['DOMAIN'] . $url, 'type' => $type, 'description' => $description, 'error_code' => $errorCode]);
+
+        /** Проверяем что результат не является null */
+        $this->assertNotNull($errorLog);
+    }
+}
