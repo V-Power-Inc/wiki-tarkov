64405df76d10954afcf916867c5aeef739495b0e
PC_Principal <basil2305@ya.ru>
2025-03-24 23:51:38 +0300

    24-03-2025г. - Back ads places

--- views/other/google-recommended.php
+++ views/other/google-recommended.php
@@ -13,6 +13,22 @@
 ?>
 <div class="col-sm-12">
     <div class="margins-vertical-20 d-block">
-        <div id="mp_custom_13082"></div> <script>window.mpsuStart = window.mpsuStart || [], window.mpsuStart.push(13082);</script>
+        <!--AdFox START-->
+        <!--Площадка: wiki-tarkov.ru / Все страницы / ADF [Place] - Блок над комментариями-->
+        <!--Категория: Основная категория-->
+        <!--Тип баннера: ADF - Блок над комментариями-->
+        <div id="adfox_167904808636315913"></div>
+        <script>
+            window.yaContextCb.push(()=>{
+                Ya.adfoxCode.create({
+                    ownerId: 700342,
+                    containerId: 'adfox_167904808636315913',
+                    params: {
+                        p1: 'cxqpp',
+                        p2: 'idnz'
+                    }
+                })
+            })
+        </script>
     </div>
 </div>
--- views/other/yandex-direct.php
+++ views/other/yandex-direct.php
@@ -8,10 +8,41 @@
  */
 ?>
 <div class="margins-vertical-20">
-    <!-- NEW ADS BLOCKS -->
-
+    <!--AdFox START-->
+    <!--Площадка: wiki-tarkov.ru / Все страницы / ADF [Place] - Боковой верхний баннер-->
+    <!--Категория: Основная категория-->
+    <!--Тип баннера: ADF - Боковой верхний баннер-->
+        <div id="adfox_167904820116868185"></div>
+        <script>
+            window.yaContextCb.push(()=>{
+                Ya.adfoxCode.create({
+                    ownerId: 700342,
+                    containerId: 'adfox_167904820116868185',
+                    params: {
+                        p1: 'cxqpr',
+                        p2: 'idnw'
+                    }
+                })
+            })
+        </script>
 </div>
 
 <div class="margins-vertical-20">
-    <!-- NEW ADS BLOCKS -->
+    <!--AdFox START-->
+    <!--Площадка: wiki-tarkov.ru / Все страницы / ADF [Place] - Боковой нижний баннер-->
+    <!--Категория: Основная категория-->
+    <!--Тип баннера: ADF - Боковой нижний баннер-->
+        <div id="adfox_16815780448977304"></div>
+        <script>
+            window.yaContextCb.push(()=>{
+                Ya.adfoxCode.create({
+                    ownerId: 700342,
+                    containerId: 'adfox_16815780448977304',
+                    params: {
+                        p1: 'cxqps',
+                        p2: 'idny'
+                    }
+                })
+            })
+        </script>
 </div>
