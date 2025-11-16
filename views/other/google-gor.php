94f6051a39f33112e7568e586722ede3476dd825
PC_Principal <basil2305@ya.ru>
2025-11-16 13:42:29 +0300

    16-11-2025г. - Отключены все рекламные блоки,оверлей и фуллскрин мобайл

--- views/other/google-gor.php
+++ views/other/google-gor.php
@@ -7,18 +7,6 @@
  * Горизонтальная реклама в верхней части сайта - Убрана 29 октября 2025
  */
 ?>
-<!--<div class="margins-vertical-20">-->
-<!--    <div id="adfox_176045507628688078"></div>-->
-<!--    <script>-->
-<!--        window.yaContextCb.push(()=>{-->
-<!--            Ya.adfoxCode.create({-->
-<!--                ownerId: 700342,-->
-<!--                containerId: 'adfox_176045507628688078',-->
-<!--                params: {-->
-<!--                    p1: 'cxqpo',-->
-<!--                    p2: 'idoa'-->
-<!--                }-->
-<!--            })-->
-<!--        })-->
-<!--    </script>-->
-<!--</div>-->
+<div class="margins-vertical-20">
+    <!-- Here code of block -->
+</div>
--- views/other/google-recommended.php
+++ views/other/google-recommended.php
@@ -14,21 +14,6 @@
 ?>
 <div class="col-sm-12">
     <div class="margins-vertical-20 d-block">
-        <!--Площадка: wiki-tarkov.ru / Все страницы / ADF [Place] - Блок над комментариями-->
-        <!--Категория: Основная категория-->
-        <!--Тип баннера: ADF - Блок над комментариями-->
-        <div id="adfox_176045599936098078"></div>
-        <script>
-            window.yaContextCb.push(()=>{
-                Ya.adfoxCode.create({
-                    ownerId: 700342,
-                    containerId: 'adfox_176045599936098078',
-                    params: {
-                        p1: 'cxqpp',
-                        p2: 'idnz'
-                    }
-                })
-            })
-        </script>
+        <!-- Here code of block -->
     </div>
 </div>
