336d292c3c3aee9b50c304c70174934edbe174b4
PC_Principal <basil2305@ya.ru>
2026-01-31 18:04:07 +0300

    31-01-2026г. - Убираю баннерные блоки

--- views/other/google-gor.php
+++ views/other/google-gor.php
@@ -4,12 +4,7 @@
  * User: comp
  * Date: 21.04.2018
  * Time: 7:59
- * Горизонтальная реклама в верхней части сайта - тестируется новый рекламодатель
+ * Горизонтальная реклама в верхней части сайта
  */
 ?>
-<div class="margins-vertical-20">
-    <div id="frame" style="width: 100%;margin: auto;position: relative; z-index: 99998;">
-        <iframe data-aa='2425987' src='//acceptable.a-ads.com/2425987/?size=Adaptive'
-                style='border:0; padding:0; width:70%; height:auto; overflow:hidden;display: block;margin: auto'></iframe>
-    </div>
-</div>
+<!-- Here code of block -->
